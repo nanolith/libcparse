fe406a7f233ccabd13a994ffb745317d0699d8d6
Justin Handville <nanolith@gmail.com>
2023-10-01 23:53:28 -0400

    decl for message_get_type.

--- include/libcparse/message.h
+++ include/libcparse/message.h
@@ -23,11 +23,26 @@ extern "C" {
 typedef struct CPARSE_SYM(message) CPARSE_SYM(message);
 
 /******************************************************************************/
+/* Start of public methods.                                                   */
+/******************************************************************************/
+
+/**
+ * \brief Get the message type for the given message.
+ *
+ * \param msg                   The message to query.
+ *
+ * \returns the \ref event_type for this event.
+ */
+int CPARSE_SYM(message_get_type)(const CPARSE_SYM(message)* msg);
+
+/******************************************************************************/
 /* Start of public exports.                                                   */
 /******************************************************************************/
 #define __INTERNAL_CPARSE_IMPORT_message_sym(sym) \
     CPARSE_BEGIN_EXPORT \
     typedef CPARSE_SYM(message) sym ## message; \
+    static inline int sym ## message_get_type(const CPARSE_SYM(message)* x) { \
+        return CPARSE_SYM(message_get_type)(x); } \
     CPARSE_END_EXPORT \
     REQUIRE_SEMICOLON_HERE
 #define CPARSE_IMPORT_message_as(sym) \
