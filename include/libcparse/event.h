b867d12096d0f84091f53498d25981fc5e76d6a8
Justin Handville <nanolith@gmail.com>
2023-10-03 19:02:03 -0400

    decl for event_init.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -24,6 +24,24 @@ extern "C" {
 typedef struct CPARSE_SYM(event) CPARSE_SYM(event);
 
 /******************************************************************************/
+/* Start of constructors.                                                     */
+/******************************************************************************/
+
+/**
+ * \brief Perform an in-place initialization of an event_type instance.
+ *
+ * \param ev                    Pointer to the event to initialize.
+ * \param event_type            The type of the event.
+ * \param cursor                The event cursor.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero failure code on failure.
+ */
+int CPARSE_SYM(event_init)(
+    CPARSE_SYM(event)* ev, int event_type, const CPARSE_SYM(cursor)* cursor);
+
+/******************************************************************************/
 /* Start of public methods.                                                   */
 /******************************************************************************/
 
