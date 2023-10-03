dcb6dcd2d6c440c4bc7726ef1e4701297b1d3cf3
Justin Handville <nanolith@gmail.com>
2023-10-03 19:13:36 -0400

    impl of event_get_cursor.

---
+++ src/event/event_get_cursor.c
@@ -0,0 +1,26 @@
+/**
+ * \file src/event/event_get_cursor.c
+ *
+ * \brief Get the event cursor.
+ *
+ * \copyright 2023 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <libcparse/status_codes.h>
+#include <string.h>
+
+#include "event_internal.h"
+
+/**
+ * \brief Get the cursor for the given event.
+ *
+ * \param ev                    The event to query.
+ *
+ * \returns the \ref cursor for this event.
+ */
+const CPARSE_SYM(cursor)*
+CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev)
+{
+    return &ev->event_cursor;
+}
