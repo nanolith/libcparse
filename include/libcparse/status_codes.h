a3d2286b3e44105d01adb58d9e6dedd5c807d67c
Justin Handville <nanolith@gmail.com>
2023-10-03 19:07:58 -0400

    decl for STATUS_SUCCESS.

---
+++ include/libcparse/status_codes.h
@@ -0,0 +1,12 @@
+/**
+ * \file libcparse/status_codes.h
+ *
+ * \brief Status codes for the libcparse library.
+ *
+ * \copyright 2023 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#define STATUS_SUCCESS                                          0
