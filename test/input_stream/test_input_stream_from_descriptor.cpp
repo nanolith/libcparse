1e900519f59d42ebac787ba5b10625179de58141
Justin Handville <nanolith@gmail.com>
2023-10-06 22:44:31 -0400

    Added unit test to read multiple characters.

--- test/input_stream/test_input_stream_from_descriptor.cpp
+++ test/input_stream/test_input_stream_from_descriptor.cpp
@@ -80,3 +80,49 @@ TEST(read_character)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == input_stream_release(stream));
 }
+
+/**
+ * We can read multiple characters from the descriptor input_stream.
+ */
+TEST(read_multiple_characters)
+{
+    const char* test_msg = "abcdef";
+    ssize_t test_msg_size = strlen(test_msg);
+    input_stream* stream = nullptr;
+    int ch;
+    int sd[2];
+
+    /* create the socket pair for this stream. */
+    TEST_ASSERT(0 == socketpair(AF_UNIX, SOCK_STREAM, 0, sd));
+
+    /* write the test message to the string. */
+    TEST_ASSERT(test_msg_size == write(sd[1], test_msg, test_msg_size));
+
+    /* close the output side so the input side gets EOF. */
+    close(sd[1]);
+
+    /* Creating a socket descriptor input stream should succeed. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == input_stream_create_from_descriptor(&stream, sd[0]));
+
+    /* We can read characters. */
+    TEST_ASSERT(STATUS_SUCCESS == input_stream_read(stream, &ch));
+    TEST_EXPECT('a' == ch);
+    TEST_ASSERT(STATUS_SUCCESS == input_stream_read(stream, &ch));
+    TEST_EXPECT('b' == ch);
+    TEST_ASSERT(STATUS_SUCCESS == input_stream_read(stream, &ch));
+    TEST_EXPECT('c' == ch);
+    TEST_ASSERT(STATUS_SUCCESS == input_stream_read(stream, &ch));
+    TEST_EXPECT('d' == ch);
+    TEST_ASSERT(STATUS_SUCCESS == input_stream_read(stream, &ch));
+    TEST_EXPECT('e' == ch);
+    TEST_ASSERT(STATUS_SUCCESS == input_stream_read(stream, &ch));
+    TEST_EXPECT('f' == ch);
+
+    /* Trying to read any more characters results in an EOF. */
+    TEST_EXPECT(
+        ERROR_LIBCPARSE_INPUT_STREAM_EOF == input_stream_read(stream, &ch));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == input_stream_release(stream));
+}
