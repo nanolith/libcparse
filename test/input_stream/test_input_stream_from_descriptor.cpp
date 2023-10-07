0aab4b81940e4e8599aff1108762367d1fea45bd
Justin Handville <nanolith@gmail.com>
2023-10-06 22:43:14 -0400

    Added unit test for reading a single character.

--- test/input_stream/test_input_stream_from_descriptor.cpp
+++ test/input_stream/test_input_stream_from_descriptor.cpp
@@ -10,6 +10,7 @@
 #include <libcparse/input_stream.h>
 #include <libcparse/status_codes.h>
 #include <minunit/minunit.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
@@ -37,6 +38,42 @@ TEST(empty_EOF)
         STATUS_SUCCESS == input_stream_create_from_descriptor(&stream, sd[0]));
 
     /* Reading from this stream returns an EOF error. */
+    TEST_EXPECT(
+        ERROR_LIBCPARSE_INPUT_STREAM_EOF == input_stream_read(stream, &ch));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == input_stream_release(stream));
+}
+
+/**
+ * We can read a character from the descriptor input_stream.
+ */
+TEST(read_character)
+{
+    const char* test_msg = "a";
+    ssize_t test_msg_size = strlen(test_msg);
+    input_stream* stream = nullptr;
+    int ch;
+    int sd[2];
+
+    /* create the socket pair for this stream. */
+    TEST_ASSERT(0 == socketpair(AF_UNIX, SOCK_STREAM, 0, sd));
+
+    /* write the test message to the string. */
+    TEST_ASSERT(test_msg_size == write(sd[1], test_msg, test_msg_size));
+
+    /* close the output side so the input side gets EOF. */
+    close(sd[1]);
+
+    /* Creating a socket descriptor input stream should succeed. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == input_stream_create_from_descriptor(&stream, sd[0]));
+
+    /* We can read a character. */
+    TEST_ASSERT(STATUS_SUCCESS == input_stream_read(stream, &ch));
+    TEST_EXPECT('a' == ch);
+
+    /* Trying to read any more characters results in an EOF. */
     TEST_EXPECT(
         ERROR_LIBCPARSE_INPUT_STREAM_EOF == input_stream_read(stream, &ch));
 
