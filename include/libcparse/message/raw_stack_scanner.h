d5d680f3769e7cf6e29cfd5d08cec2a2f441f2f7
Justin Handville <nanolith@gmail.com>
2023-10-08 03:09:29 -0400

    Fixed typos in export.

--- include/libcparse/message/raw_stack_scanner.h
+++ include/libcparse/message/raw_stack_scanner.h
@@ -145,10 +145,10 @@ CPARSE_SYM(message)* CPARSE_SYM(message_rss_add_input_stream_upcast)(
         CPARSE_SYM(message_rss_add_input_stream)** x, \
         CPARSE_SYM(message)* y) { \
             return \
-                CPARSE_SYM(message_downcast_to_message_rss_add_input_stream( \
+                CPARSE_SYM(message_downcast_to_message_rss_add_input_stream)( \
                     x,y); } \
     static inline CPARSE_SYM(message)* \
-    CPARSE_SYM(message_rss_add_input_stream_upcast)( \
+    sym ## message_rss_add_input_stream_upcast( \
         CPARSE_SYM(message_rss_add_input_stream)* x) { \
             return CPARSE_SYM(message_rss_add_input_stream_upcast)(x); } \
     CPARSE_END_EXPORT \
