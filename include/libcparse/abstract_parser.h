23b9660f24a910a0323b006671c818b2de452ab6
Justin Handville <nanolith@gmail.com>
2023-10-12 23:00:47 -0400

    abstract_parser_dispose export.

--- include/libcparse/abstract_parser.h
+++ include/libcparse/abstract_parser.h
@@ -146,6 +146,9 @@ CPARSE_SYM(abstract_parser_raw_stack_scanner_subscribe)(
     static inline int sym ## abstract_parser_init( \
         CPARSE_SYM(abstract_parser)* x, const CPARSE_SYM(message_handler)* y) {\
             return CPARSE_SYM(abstract_parser_init)(x,y); } \
+    static inline int sym ## abstract_parser_dispose( \
+        CPARSE_SYM(abstract_parser)* x) { \
+            return CPARSE_SYM(abstract_parser_dispose)(x); } \
     CPARSE_END_EXPORT \
     REQUIRE_SEMICOLON_HERE
 #define CPARSE_IMPORT_abstract_parser_as(sym) \
