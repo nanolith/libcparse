8f206bf6c2afacfe3b0d3e9202b5d02785e78698
Justin Handville <nanolith@gmail.com>
2023-10-15 01:49:53 -0400

    impl of event_raw_character_init.

---
+++ src/event/event_raw_character_init.c
@@ -0,0 +1,55 @@
+/**
+ * \file src/event/event_raw_character_init.c
+ *
+ * \brief Init method for the \ref event_raw_character type.
+ *
+ * \copyright 2023 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <libcparse/event.h>
+#include <libcparse/event/raw_character.h>
+#include <libcparse/status_codes.h>
+#include <string.h>
+
+CPARSE_IMPORT_event;
+
+/**
+ * \brief Perform an in-place initialization of an \ref event_raw_character
+ * instance.
+ *
+ * \param ev                Pointer to the event to initialize.
+ * \param event_type        The type of the event.
+ * \param cursor            The event cursor.
+ * \param ch                The raw character for this event.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero failure code on failure.
+ */
+int CPARSE_SYM(event_raw_character_init)(
+    CPARSE_SYM(event_raw_character)* ev, int event_type,
+    const CPARSE_SYM(cursor)* cursor, int ch)
+{
+    int retval;
+
+    /* clear event memory. */
+    memset(ev, 0, sizeof(*ev));
+
+    /* initialize base type. */
+    retval = event_init(&ev->hdr, event_type, cursor);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* initialize instance variables. */
+    ev->raw_character = ch;
+
+    /* success. */
+    retval = STATUS_SUCCESS;
+    goto done;
+
+done:
+    return retval;
+}
