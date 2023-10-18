f064be3a01c3f3fce0dafc921d7ac8c0c0662f8d
Justin Handville <nanolith@gmail.com>
2023-10-17 20:10:19 -0400

    decl of raw_stack_entry_create.

--- src/raw_stack_scanner/raw_stack_scanner_internal.h
+++ src/raw_stack_scanner/raw_stack_scanner_internal.h
@@ -24,6 +24,7 @@ struct CPARSE_SYM(raw_stack_entry)
 {
     CPARSE_SYM(raw_stack_entry)* next;
     CPARSE_SYM(input_stream)* stream;
+    char* name;
     CPARSE_SYM(cursor) pos;
 };
 
@@ -32,6 +33,30 @@ struct CPARSE_SYM(raw_stack_scanner)
     CPARSE_SYM(abstract_parser) hdr;
     CPARSE_SYM(raw_stack_entry)* head;
 };
+
+/******************************************************************************/
+/* Start of constructors.                                                     */
+/******************************************************************************/
+
+/**
+ * \brief Create a \ref raw_stack_entry instance with the given stream and
+ * name.
+ *
+ * \note Ownership of \p stream passes to this instance on success. The stream
+ * will be released when this instance is released.
+ *
+ * \param ent               Pointer to the \ref raw_stack_entry pointer to
+ *                          receive this instance on success.
+ * \param stream            The input stream for this instance.
+ * \param name              The name of this stream.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(raw_stack_entry_create)(
+    CPARSE_SYM(raw_stack_entry)** ent, CPARSE_SYM(input_stream)* stream,
+    const char* name);
 
 /* C++ compatibility. */
 # ifdef   __cplusplus
