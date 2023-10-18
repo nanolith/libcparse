52071fa0ddf694786ab3d52af8a49c26e3f166c6
Justin Handville <nanolith@gmail.com>
2023-10-17 20:22:18 -0400

    decl for event_reactor_add.

--- include/libcparse/event_reactor.h
+++ include/libcparse/event_reactor.h
@@ -10,6 +10,7 @@
 
 #pragma once
 
+#include <libcparse/event_handler_fwd.h>
 #include <libcparse/function_decl.h>
 
 /* C++ compatibility. */
@@ -48,6 +49,23 @@ int CPARSE_SYM(event_reactor_create)(CPARSE_SYM(event_reactor)** er);
  *      - a non-zero error code on failure.
  */
 int CPARSE_SYM(event_reactor_release)(CPARSE_SYM(event_reactor)* er);
+
+/******************************************************************************/
+/* Start of public methods.                                                   */
+/******************************************************************************/
+
+/**
+ * \brief Add an \ref event_handler to the event reactor instance.
+ *
+ * \param er                The event reactor for this operation.
+ * \param eh                The event handler to add to this reactor.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(event_reactor_add)(
+    CPARSE_SYM(event_reactor)* er, CPARSE_SYM(event_handler)* eh);
 
 /* C++ compatibility. */
 # ifdef   __cplusplus
