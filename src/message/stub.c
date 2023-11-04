93f1273b6047ed61e08fe7569e07cd3395732f89
Justin Handville <nanolith@gmail.com>
2023-11-03 22:08:15 -0400

    removed unneded stub.

--- src/message/stub.c
+++
@@ -1,8 +0,0 @@
-#include <libcparse/message.h>
-#include <libcparse/message/run.h>
-#include <libcparse/message/subscription.h>
-#include <stdio.h>
-
-CPARSE_IMPORT_message;
-CPARSE_IMPORT_message_run;
-CPARSE_IMPORT_message_subscription;
