427f92aea79e8408320e617671bf68be512111e1
Justin Handville <nanolith@gmail.com>
2023-11-03 22:26:44 -0400

    comment sub messages are valid sub messages.

--- src/message_type/message_type_valid_subscription_type.c
+++ src/message_type/message_type_valid_subscription_type.c
@@ -23,6 +23,7 @@ bool CPARSE_SYM(message_type_valid_subscription_type)(int msg_type)
         /* valid message types. */
         case CPARSE_MESSAGE_TYPE_RSS_SUBSCRIBE:
         case CPARSE_MESSAGE_TYPE_RFLO_SUBSCRIBE:
+        case CPARSE_MESSAGE_TYPE_COMMENT_SCANNER_SUBSCRIBE:
             return true;
 
         default:
