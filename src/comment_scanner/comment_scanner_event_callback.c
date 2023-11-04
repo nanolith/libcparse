0c12414316d0fc4b531332f98ea45dc7295cd5ed
Justin Handville <nanolith@gmail.com>
2023-11-03 22:31:46 -0400

    Rebroadcast EOF events.

--- src/comment_scanner/comment_scanner_event_callback.c
+++ src/comment_scanner/comment_scanner_event_callback.c
@@ -9,12 +9,14 @@
 
 #include <libcparse/comment_scanner.h>
 #include <libcparse/event.h>
+#include <libcparse/event_reactor.h>
 #include <libcparse/status_codes.h>
 
 #include "comment_scanner_internal.h"
 
 CPARSE_IMPORT_comment_scanner;
 CPARSE_IMPORT_event;
+CPARSE_IMPORT_event_reactor;
 
 /**
  * \brief Event handler callback for \ref comment_scanner.
@@ -30,10 +32,13 @@ CPARSE_IMPORT_event;
 int CPARSE_SYM(comment_scanner_event_callback)(
     void* context, const CPARSE_SYM(event)* ev)
 {
-    (void)context;
+    comment_scanner* scanner = (comment_scanner*)context;
 
     switch (event_get_type(ev))
     {
+        case CPARSE_EVENT_TYPE_EOF:
+            return event_reactor_broadcast(scanner->reactor, ev);
+
         default:
             return STATUS_SUCCESS;
     }
