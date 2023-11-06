951e78b8326f8adec9ff6d06299bcd3015667e2f
Justin Handville <nanolith@gmail.com>
2023-11-05 19:53:44 -0500

    comment_scanner slash/eof unit test.

--- test/comment_scanner/test_comment_scanner.cpp
+++ test/comment_scanner/test_comment_scanner.cpp
@@ -308,3 +308,61 @@ TEST(input_stream_block_comment_dupes)
     TEST_ASSERT(STATUS_SUCCESS == comment_scanner_release(scanner));
     TEST_ASSERT(STATUS_SUCCESS == event_handler_dispose(&eh));
 }
+
+/**
+ * Test that slash-eof is processed correctly.
+ */
+TEST(input_stream_slash_eof)
+{
+    comment_scanner* scanner;
+    input_stream* stream;
+    event_handler eh;
+    test_context t1;
+    const char* TEST_STRING = "/";
+    const char* TEST_STRING_XFORM = "/";
+
+    /* create the scanner instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == comment_scanner_create(&scanner));
+
+    /* create our event handler. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_handler_init(&eh, &dummy_callback, &t1));
+
+    /* get the abstract parser. */
+    auto ap = comment_scanner_upcast(scanner);
+
+    /* subscribe to the scanner. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == abstract_parser_comment_scanner_subscribe(ap, &eh));
+
+    /* create an input stream. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == input_stream_create_from_string(&stream, TEST_STRING));
+
+    /* add the input stream to the parser. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == abstract_parser_push_input_stream(ap, "stdin", stream));
+
+    /* precondition: eof is false. */
+    TEST_ASSERT(!t1.eof);
+
+    /* run the filter. */
+    TEST_ASSERT(STATUS_SUCCESS == abstract_parser_run(ap));
+
+    /* postcondition: eof is true. */
+    TEST_EXPECT(t1.eof);
+
+    /* postcondition: vals is not empty. */
+    TEST_EXPECT(!t1.vals.empty());
+
+    /* postcondition: vals matches our string. */
+    string out(t1.vals.begin(), t1.vals.end());
+    TEST_EXPECT(out == TEST_STRING_XFORM);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == comment_scanner_release(scanner));
+    TEST_ASSERT(STATUS_SUCCESS == event_handler_dispose(&eh));
+}
