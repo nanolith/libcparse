df779f9b9e7b5bdddb513a60e02e4d4e965d303a
Justin Handville <nanolith@gmail.com>
2023-11-07 00:29:24 -0500

    Added line comment support to comment scanner.

--- src/comment_scanner/comment_scanner_event_callback.c
+++ src/comment_scanner/comment_scanner_event_callback.c
@@ -32,10 +32,16 @@ static int process_char_event_block(
     comment_scanner* scanner, const event_raw_character* ev, int ch);
 static int process_char_event_block_star(
     comment_scanner* scanner, const event_raw_character* ev, int ch);
+static int process_char_event_line(
+    comment_scanner* scanner, const event_raw_character* ev, int ch);
 static int process_eof_event(comment_scanner* scanner, const event* ev);
 static int begin_block_comment_broadcast(
     comment_scanner* scanner, const event_raw_character* ev);
 static int end_block_comment_broadcast(
+    comment_scanner* scanner, const event_raw_character* ev);
+static int begin_line_comment_broadcast(
+    comment_scanner* scanner, const event_raw_character* ev);
+static int end_line_comment_broadcast(
     comment_scanner* scanner, const event_raw_character* ev);
 static int raw_character_broadcast(
     comment_scanner* scanner, const cursor* pos, int ch);
@@ -168,6 +174,10 @@ static int process_char_event(comment_scanner* scanner, const event* ev)
             retval = process_char_event_block_star(scanner, rev, ch);
             goto done;
 
+        case CPARSE_COMMENT_SCANNER_STATE_IN_LINE_COMMENT:
+            retval = process_char_event_line(scanner, rev, ch);
+            goto done;
+
         /* TODO - fix this up. */
         default:
             retval = process_char_event_init(scanner, rev, ch);
@@ -245,26 +255,8 @@ static int process_char_event_slash(
             return begin_block_comment_broadcast(scanner, ev);
 
         case '/':
-            /* send the previous slash event to all subscribers. */
-            retval = raw_character_broadcast(scanner, &scanner->pos1, '/');
-            if (STATUS_SUCCESS != retval)
-            {
-                return retval;
-            }
-
-            /* cache the current position. */
-            retval =
-                comment_scanner_cached_file_position_set(
-                    scanner,
-                    event_get_cursor(event_raw_character_upcast(
-                        (event_raw_character*)ev)));
-            if (STATUS_SUCCESS != retval)
-            {
-                return retval;
-            }
-            /* we are now in the slash state. */
-            scanner->state = CPARSE_COMMENT_SCANNER_STATE_SLASH;
-            return STATUS_SUCCESS;
+            scanner->state = CPARSE_COMMENT_SCANNER_STATE_IN_LINE_COMMENT;
+            return begin_line_comment_broadcast(scanner, ev);
 
         default:
             /* send the slash event to all subscribers. */
@@ -393,6 +385,36 @@ static int process_char_event_block_star(
 }
 
 /**
+ * \brief Process a char event in line state.
+ *
+ * \param scanner           The \ref comment_scanner for this operation.
+ * \param ev                The raw character event to process.
+ * \param ch                The character to process.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int process_char_event_line(
+    comment_scanner* scanner, const event_raw_character* ev, int ch)
+{
+    /* decode the character. */
+    switch (ch)
+    {
+        case '\n':
+            scanner->state = CPARSE_COMMENT_SCANNER_STATE_INIT;
+            return end_line_comment_broadcast(scanner, ev);
+
+        default:
+            /* broadcast this event to all subscribers. */
+            return
+                event_reactor_broadcast(
+                    scanner->reactor,
+                    event_raw_character_upcast((event_raw_character*)ev));
+    }
+}
+
+/**
  * \brief Broadcast a begin block comment event.
  *
  * \param scanner           The \ref comment_scanner for this operation.
@@ -497,6 +519,128 @@ static int end_block_comment_broadcast(
 
 cleanup_bev:
     release_retval = event_dispose(&bev);
+    if (STATUS_SUCCESS != release_retval)
+    {
+        retval = release_retval;
+    }
+
+done:
+    memset(&pos, 0, sizeof(pos));
+
+    return retval;
+}
+
+/**
+ * \brief Broadcast a begin line comment event.
+ *
+ * \param scanner           The \ref comment_scanner for this operation.
+ * \param ev                The raw character event for the star.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int begin_line_comment_broadcast(
+    comment_scanner* scanner, const event_raw_character* ev)
+{
+    int retval, release_retval;
+    cursor pos;
+    event lev;
+
+    const cursor* slashpos =
+        event_get_cursor(event_raw_character_upcast((event_raw_character*)ev));
+
+    /* copy the cached position. */
+    memcpy(&pos, &scanner->pos1, sizeof(pos));
+
+    /* update the end column and line. */
+    pos.end_line = slashpos->end_line;
+    pos.end_col = slashpos->end_col;
+
+    /* initialize the begin line comment event. */
+    retval = event_init(&lev, CPARSE_EVENT_TYPE_COMMENT_LINE_BEGIN, &pos);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* broadcast this message. */
+    retval = event_reactor_broadcast(scanner->reactor, &lev);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto cleanup_lev;
+    }
+
+    /* success. */
+    retval = STATUS_SUCCESS;
+    goto cleanup_lev;
+
+cleanup_lev:
+    release_retval = event_dispose(&lev);
+    if (STATUS_SUCCESS != release_retval)
+    {
+        retval = release_retval;
+    }
+
+done:
+    memset(&pos, 0, sizeof(pos));
+
+    return retval;
+}
+
+/**
+ * \brief Broadcast an end line comment event.
+ *
+ * \param scanner           The \ref comment_scanner for this operation.
+ * \param ev                The raw character event for the star.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int end_line_comment_broadcast(
+    comment_scanner* scanner, const event_raw_character* ev)
+{
+    int retval, release_retval;
+    cursor pos;
+    event lev;
+
+    const cursor* newlinepos =
+        event_get_cursor(event_raw_character_upcast((event_raw_character*)ev));
+
+    /* copy the cached position. */
+    memcpy(&pos, newlinepos, sizeof(pos));
+
+    /* initialize the end line comment event. */
+    retval = event_init(&lev, CPARSE_EVENT_TYPE_COMMENT_LINE_END, &pos);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* broadcast this message. */
+    retval = event_reactor_broadcast(scanner->reactor, &lev);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto cleanup_lev;
+    }
+
+    /* rebroadcast the newline event. */
+    retval =
+        event_reactor_broadcast(
+            scanner->reactor,
+            event_raw_character_upcast((event_raw_character*)ev));
+    if (STATUS_SUCCESS != retval)
+    {
+        goto cleanup_lev;
+    }
+
+    /* success. */
+    retval = STATUS_SUCCESS;
+    goto cleanup_lev;
+
+cleanup_lev:
+    release_retval = event_dispose(&lev);
     if (STATUS_SUCCESS != release_retval)
     {
         retval = release_retval;
