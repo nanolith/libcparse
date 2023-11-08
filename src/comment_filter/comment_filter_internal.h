a3bf3144ae9ea0c7a46d1861f2e647d258dc4614
Justin Handville <nanolith@gmail.com>
2023-11-08 00:30:24 -0500

    comment_filter impl.

---
+++ src/comment_filter/comment_filter_internal.h
@@ -0,0 +1,36 @@
+/**
+ * \file comment_filter/comment_filter_internal.h
+ *
+ * \brief Internal declarations for \ref comment_filter.
+ *
+ * \copyright 2023 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <libcparse/comment_scanner.h>
+#include <libcparse/event_reactor_fwd.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+typedef struct CPARSE_SYM(comment_filter)
+CPARSE_SYM(comment_filter);
+
+struct CPARSE_SYM(comment_filter)
+{
+    CPARSE_SYM(comment_scanner)* parent;
+    CPARSE_SYM(event_reactor)* reactor;
+    CPARSE_SYM(message_handler) parent_mh;
+    char* file;
+    CPARSE_SYM(cursor) pos1;
+    int state;
+};
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
--- src/comment_filter/stub.c
+++ src/comment_filter/stub.c
@@ -1,4 +1,6 @@
 #include <libcparse/comment_filter.h>
 #include <stdio.h>
 
+#include "comment_filter_internal.h"
+
 CPARSE_IMPORT_comment_filter;
