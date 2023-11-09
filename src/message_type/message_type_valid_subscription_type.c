9a7c6fd889407393c6e5413c29598eaa475d64b9
Justin Handville <nanolith@gmail.com>
2023-11-09 01:11:21 -0500

    Added comment filter as a valid subscription type.

--- src/message_type/message_type_valid_subscription_type.c
+++ src/message_type/message_type_valid_subscription_type.c
@@ -24,6 +24,7 @@ bool CPARSE_SYM(message_type_valid_subscription_type)(int msg_type)
         case CPARSE_MESSAGE_TYPE_RSS_SUBSCRIBE:
         case CPARSE_MESSAGE_TYPE_RFLO_SUBSCRIBE:
         case CPARSE_MESSAGE_TYPE_COMMENT_SCANNER_SUBSCRIBE:
+        case CPARSE_MESSAGE_TYPE_COMMENT_FILTER_SUBSCRIBE:
             return true;
 
         default:
