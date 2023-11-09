404917435b6d14cf0d1056b46d3a20794edda22a
Justin Handville <nanolith@gmail.com>
2023-11-09 01:18:04 -0500

    Refactored event callback to extract eof handler.

--- src/comment_filter/comment_filter_event_callback.c
+++ src/comment_filter/comment_filter_event_callback.c
@@ -19,6 +19,7 @@ CPARSE_IMPORT_event;
 CPARSE_IMPORT_event_reactor;
 
 static int process_char_event(comment_filter* filter, const event* ev);
+static int process_eof_event(comment_filter* filter, const event* ev);
 
 /**
  * \brief Event handler callback for \ref comment_filter.
@@ -39,7 +40,7 @@ int CPARSE_SYM(comment_filter_event_callback)(
     switch (event_get_type(ev))
     {
         case CPARSE_EVENT_TYPE_EOF:
-            return event_reactor_broadcast(filter->reactor, ev);
+            return process_eof_event(filter, ev);
 
         case CPARSE_EVENT_TYPE_RAW_CHARACTER:
             return process_char_event(filter, ev);
@@ -49,6 +50,20 @@ int CPARSE_SYM(comment_filter_event_callback)(
     }
 }
 
+/**
+ * \brief Process an eof event.
+ *
+ * \param filter            The \ref comment_filter for this operation.
+ * \param ev                The raw character event to process.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int process_eof_event(comment_filter* filter, const event* ev)
+{
+    return event_reactor_broadcast(filter->reactor, ev);
+}
 /**
  * \brief Process a raw character event.
  *
