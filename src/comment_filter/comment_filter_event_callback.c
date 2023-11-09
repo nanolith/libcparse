3043fa089fed3b0d727f72dfc7effd8be28250ac
Justin Handville <nanolith@gmail.com>
2023-11-09 01:13:43 -0500

    Rebroadcast EOF events.

--- src/comment_filter/comment_filter_event_callback.c
+++ src/comment_filter/comment_filter_event_callback.c
@@ -7,9 +7,16 @@
  * distribution for the license terms under which this software is distributed.
  */
 
+#include <libcparse/comment_filter.h>
+#include <libcparse/event.h>
+#include <libcparse/event_reactor.h>
 #include <libcparse/status_codes.h>
 
 #include "comment_filter_internal.h"
+
+CPARSE_IMPORT_comment_filter;
+CPARSE_IMPORT_event;
+CPARSE_IMPORT_event_reactor;
 
 /**
  * \brief Event handler callback for \ref comment_filter.
@@ -25,8 +32,14 @@
 int CPARSE_SYM(comment_filter_event_callback)(
     void* context, const CPARSE_SYM(event)* ev)
 {
-    (void)context;
-    (void)ev;
+    comment_filter* filter = (comment_filter*)context;
 
-    return STATUS_SUCCESS;
+    switch (event_get_type(ev))
+    {
+        case CPARSE_EVENT_TYPE_EOF:
+            return event_reactor_broadcast(filter->reactor, ev);
+
+        default:
+            return STATUS_SUCCESS;
+    }
 }
