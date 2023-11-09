65cad66779245e8bdfb82efe231505e39ae163d0
Justin Handville <nanolith@gmail.com>
2023-11-09 00:50:55 -0500

    comment_filter_cached_file_position_set export.

--- src/comment_filter/comment_filter_internal.h
+++ src/comment_filter/comment_filter_internal.h
@@ -109,6 +109,9 @@ int CPARSE_SYM(comment_filter_cached_file_position_set)(
     static inline int sym ## comment_filter_cached_file_clear( \
         CPARSE_SYM(comment_filter)* x) { \
             return CPARSE_SYM(comment_filter_cached_file_clear)(x); } \
+    static inline int sym ## comment_filter_cached_file_position_set( \
+        CPARSE_SYM(comment_filter)* x, const CPARSE_SYM(cursor)* y) { \
+            return CPARSE_SYM(comment_filter_cached_file_position_set)(x,y); } \
     CPARSE_END_EXPORT \
     REQUIRE_SEMICOLON_HERE
 #define CPARSE_IMPORT_comment_filter_internal_as(sym) \
