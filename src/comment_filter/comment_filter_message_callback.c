471733fcc9bc4db84f54135dd3583e704a638a2e
Justin Handville <nanolith@gmail.com>
2023-11-09 01:08:52 -0500

    Handle comment filter subscription messages.

--- src/comment_filter/comment_filter_message_callback.c
+++ src/comment_filter/comment_filter_message_callback.c
@@ -7,9 +7,24 @@
  * distribution for the license terms under which this software is distributed.
  */
 
+#include <libcparse/comment_filter.h>
+#include <libcparse/event_handler.h>
+#include <libcparse/event_reactor.h>
+#include <libcparse/message.h>
+#include <libcparse/message/subscription.h>
+#include <libcparse/message_handler.h>
 #include <libcparse/status_codes.h>
 
 #include "comment_filter_internal.h"
+
+CPARSE_IMPORT_comment_filter;
+CPARSE_IMPORT_event_handler;
+CPARSE_IMPORT_event_reactor;
+CPARSE_IMPORT_message;
+CPARSE_IMPORT_message_handler;
+CPARSE_IMPORT_message_subscription;
+
+static int subscribe(comment_filter* filter, const message* msg);
 
 /**
  * \brief Message handler callback for \ref comment_filter.
@@ -25,8 +40,55 @@
 int CPARSE_SYM(comment_filter_message_callback)(
     void* context, const CPARSE_SYM(message)* msg)
 {
-    (void)context;
-    (void)msg;
+    comment_filter* filter = (comment_filter*)context;
 
-    return ERROR_LIBCPARSE_UNHANDLED_MESSAGE;
+    switch (message_get_type(msg))
+    {
+        case CPARSE_MESSAGE_TYPE_COMMENT_FILTER_SUBSCRIBE:
+            return subscribe(filter, msg);
+
+        default:
+            return message_handler_send(&filter->parent_mh, msg);
+    }
+}
+
+/**
+ * \brief Subscribe to the comment_filter.
+ *
+ * \param filter            The filter for this operation.
+ * \param msg               The message for this operation.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int subscribe(comment_filter* filter, const message* msg)
+{
+    int retval;
+    message_subscribe* m;
+    const event_handler* eh;
+
+    /* dynamic cast the message. */
+    retval = message_downcast_to_message_subscribe(&m, (message*)msg);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* get the event handler for this message. */
+    eh = message_subscribe_event_handler_get(m);
+
+    /* add this handler to our reactor. */
+    retval = event_reactor_add(filter->reactor, eh);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* success. */
+    retval = STATUS_SUCCESS;
+    goto done;
+
+done:
+    return retval;
 }
