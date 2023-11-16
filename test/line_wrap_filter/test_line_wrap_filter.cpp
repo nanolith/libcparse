0f3432aafd5e6883bb22d85a15c2eb703c202505
Justin Handville <nanolith@gmail.com>
2023-11-16 00:16:22 -0500

    line_wrap_filter newline eof unit test.

--- test/line_wrap_filter/test_line_wrap_filter.cpp
+++ test/line_wrap_filter/test_line_wrap_filter.cpp
@@ -7,22 +7,31 @@
  * distribution for the license terms under which this software is distributed.
  */
 
+#include <libcparse/event/raw_character.h>
+#include <libcparse/input_stream.h>
 #include <libcparse/line_wrap_filter.h>
 #include <libcparse/status_codes.h>
+#include <list>
 #include <minunit/minunit.h>
+#include <string>
 
 using namespace std;
 
 CPARSE_IMPORT_abstract_parser;
+CPARSE_IMPORT_cursor;
 CPARSE_IMPORT_event;
 CPARSE_IMPORT_event_handler;
+CPARSE_IMPORT_event_raw_character;
+CPARSE_IMPORT_input_stream;
 CPARSE_IMPORT_line_wrap_filter;
 
 TEST_SUITE(line_wrap_filter);
 
 struct test_context
 {
+    list<int> vals;
     bool eof;
+    cursor pos;
 
     test_context()
         : eof(false)
@@ -32,11 +41,24 @@ struct test_context
 
 static int dummy_callback(void* context, const CPARSE_SYM(event)* ev)
 {
+    int retval;
     test_context* ctx = (test_context*)context;
 
     if (CPARSE_EVENT_TYPE_EOF == event_get_type(ev))
     {
         ctx->eof = true;
+    }
+    else if (CPARSE_EVENT_TYPE_RAW_CHARACTER == event_get_type(ev))
+    {
+        event_raw_character* rev;
+        retval = event_downcast_to_event_raw_character(&rev, (event*)ev);
+        if (STATUS_SUCCESS != retval)
+        {
+            return retval;
+        }
+
+        ctx->vals.push_back(event_raw_character_get(rev));
+        memcpy(&ctx->pos, event_get_cursor(ev), sizeof(ctx->pos));
     }
     else
     {
@@ -91,6 +113,59 @@ TEST(subscribe)
 
     /* postcondition: eof is true. */
     TEST_EXPECT(t1.eof);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == line_wrap_filter_release(filter));
+}
+
+/**
+ * Test that a string with no line wraps is passed correctly.
+ */
+TEST(no_line_wraps)
+{
+    line_wrap_filter* filter;
+    input_stream* stream;
+    event_handler eh;
+    test_context t1;
+    const char* INPUT_STRING = "abc\n123";
+    const char* OUTPUT_STRING = "abc\n123\n";
+
+    /* we can create a line_wrap_filter. */
+    TEST_ASSERT(STATUS_SUCCESS == line_wrap_filter_create(&filter));
+
+    /* initialize our event handler. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_handler_init(&eh, &dummy_callback, &t1));
+
+    /* get the abstract parser. */
+    auto ap = line_wrap_filter_upcast(filter);
+
+    /* subscribe to the filter. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == abstract_parser_line_wrap_filter_subscribe(ap, &eh));
+
+    /* create an input stream. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == input_stream_create_from_string(&stream, INPUT_STRING));
+
+    /* add the input stream to the parser. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == abstract_parser_push_input_stream(ap, "stdin", stream));
+
+    /* precondition: eof is false. */
+    TEST_ASSERT(!t1.eof);
+
+    /* run the filter. */
+    TEST_ASSERT(STATUS_SUCCESS == abstract_parser_run(ap));
+
+    /* postcondition: eof is true. */
+    TEST_EXPECT(t1.eof);
+
+    /* postcondition: vals matches our output string. */
+    string out(t1.vals.begin(), t1.vals.end());
+    TEST_EXPECT(out == OUTPUT_STRING);
 
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == line_wrap_filter_release(filter));
