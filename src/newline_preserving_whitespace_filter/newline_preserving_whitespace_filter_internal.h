e748d63e9e44441464efbdfe4518b431a9696bbb
Justin Handville <nanolith@gmail.com>
2023-11-22 22:46:42 -0500

    newline_preserving_whitespace_filter impl.

---
+++ src/newline_preserving_whitespace_filter/newline_preserving_whitespace_filter_internal.h
@@ -0,0 +1,37 @@
+/**
+ * \file libcparse/newline_preserving_whitespace_filter_internal.h
+ *
+ * \brief Internal declarations and definitions for the newline preserving
+ * whitespace filter.
+ *
+ * \copyright 2023 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <libcparse/comment_filter.h>
+#include <libcparse/event_reactor_fwd.h>
+#include <libcparse/file_position_cache.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+typedef struct CPARSE_SYM(newline_preserving_whitespace_filter_internal)
+CPARSE_SYM(newline_preserving_whitespace_filter_internal);
+
+struct CPARSE_SYM(newline_preserving_whitespace_filter_internal)
+{
+    CPARSE_SYM(comment_filter)* parent;
+    CPARSE_SYM(event_reactor)* reactor;
+    CPARSE_SYM(message_handler) parent_mh;
+    CPARSE_SYM(file_position_cache)* cache;
+    int state;
+};
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
