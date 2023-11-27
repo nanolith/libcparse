654c2460c90f72a7fddb5d07491ddff978fc56a4
Justin Handville <nanolith@gmail.com>
2023-11-26 21:55:50 -0500

    Basic raw character passthrough for filter.

--- src/newline_preserving_whitespace_filter/newline_preserving_whitespace_filter_event_callback.c
+++ src/newline_preserving_whitespace_filter/newline_preserving_whitespace_filter_event_callback.c
@@ -19,6 +19,8 @@ CPARSE_IMPORT_event;
 CPARSE_IMPORT_event_reactor;
 CPARSE_IMPORT_newline_preserving_whitespace_filter;
 
+static int process_char_event(
+    newline_preserving_whitespace_filter* filter, const event* ev);
 static int process_eof_event(
     newline_preserving_whitespace_filter* filter, const event* ev);
 
@@ -45,6 +47,9 @@ int CPARSE_SYM(newline_preserving_whitespace_filter_event_callback)(
         case CPARSE_EVENT_TYPE_EOF:
             return process_eof_event(filter, ev);
 
+        case CPARSE_EVENT_TYPE_RAW_CHARACTER:
+            return process_char_event(filter, ev);
+
         default:
             return STATUS_SUCCESS;
     }
@@ -53,7 +58,7 @@ int CPARSE_SYM(newline_preserving_whitespace_filter_event_callback)(
 /**
  * \brief Process an eof event.
  *
- * \param filter            The \ref comment_filter for this operation.
+ * \param filter            The filter for this operation.
  * \param ev                The raw character event to process.
  *
  * \returns a status code indicating success or failure.
@@ -64,4 +69,28 @@ static int process_eof_event(
     newline_preserving_whitespace_filter* filter, const event* ev)
 {
     return event_reactor_broadcast(filter->reactor, ev);
+}
+
+/**
+ * \brief Process a raw character event.
+ *
+ * \param filter            The filter for this operation.
+ * \param ev                The raw character event to process.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int process_char_event(
+    newline_preserving_whitespace_filter* filter, const event* ev)
+{
+    switch (filter->state)
+    {
+        /* forward non-comment characters. */
+        case CPARSE_NL_WHITESPACE_FILTER_STATE_INIT:
+            return event_reactor_broadcast(filter->reactor, ev);
+
+        default:
+            return ERROR_LIBCPARSE_WHITESPACE_BAD_STATE;
+    }
 }
