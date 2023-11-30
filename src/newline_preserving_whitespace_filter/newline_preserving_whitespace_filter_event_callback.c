26c136c320243155d81a22ef21208453d1847f20
Justin Handville <nanolith@gmail.com>
2023-11-29 20:23:25 -0500

    Fixed function documentation.

--- src/newline_preserving_whitespace_filter/newline_preserving_whitespace_filter_event_callback.c
+++ src/newline_preserving_whitespace_filter/newline_preserving_whitespace_filter_event_callback.c
@@ -646,7 +646,7 @@ static int newline_eof_transition(
  * \brief Transition from the newline state to the init state.
  *
  * \param filter            The filter for this operation.
- * \param pos               The cursor position for the whitespace event.
+ * \param ev                The raw character event for this operation.
  *
  * \returns a status code indicating success or failure.
  *      - STATUS_SUCCESS on success.
