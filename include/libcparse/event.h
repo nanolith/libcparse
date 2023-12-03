2d8d576d01248654f53dd2f442e2e30ccec3e05b
Justin Handville <nanolith@gmail.com>
2023-12-03 17:36:41 -0500

    event_init_for_comment_block_begin decl.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -43,6 +43,20 @@ int CPARSE_SYM(event_init)(
     CPARSE_SYM(event)* ev, int event_type, const CPARSE_SYM(cursor)* cursor);
 
 /**
+ * \brief Perform an in-place initialization of a comment block begin event
+ * instance.
+ *
+ * \param ev                    Pointer to the event to initialize.
+ * \param cursor                The event cursor.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(event_init_for_comment_block_begin)(
+    CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
+
+/**
  * \brief Perform an in-place disposal of an event instance.
  *
  * \param ev                    Pointer to the event to dispose.
