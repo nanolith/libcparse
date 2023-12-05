be3ff9da25e1d6e1a27746a625e055ef4cca21f4
Justin Handville <nanolith@gmail.com>
2023-12-04 23:32:19 -0500

    event_init_for_whitespace_token export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -184,6 +184,9 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
     static inline int sym ## event_init_for_eof( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_eof)(x,y); } \
+    static inline int sym ## event_init_for_whitespace_token( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return CPARSE_SYM(event_init_for_whitespace_token)(x,y); } \
     static inline int sym ## event_init_for_comment_block_begin( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_comment_block_begin)(x,y); } \
