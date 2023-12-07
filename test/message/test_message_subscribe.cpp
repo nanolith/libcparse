8c7589d85d4bd6d87f1448a8ca3a1e1ecd125e16
Justin Handville <nanolith@gmail.com>
2023-12-06 22:54:35 -0500

    Use comment scanner subscribe ctor.

--- test/message/test_message_subscribe.cpp
+++ test/message/test_message_subscribe.cpp
@@ -166,9 +166,7 @@ TEST(upcast_downcast_CPARSE_MESSAGE_TYPE_COMMENT_SCANNER_SUBSCRIBE)
     /* we can initialize the message. */
     TEST_ASSERT(
         STATUS_SUCCESS
-            == message_subscribe_init(
-                    &msg, CPARSE_MESSAGE_TYPE_COMMENT_SCANNER_SUBSCRIBE,
-                    &handler));
+            == message_subscribe_init_for_comment_scanner(&msg, &handler));
 
     /* we can upcast the message. */
     upcast_msg = message_subscribe_upcast(&msg);
