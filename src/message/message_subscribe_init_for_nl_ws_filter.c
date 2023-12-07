5df4a1e46821ef3c115314f7e932c1645f3bd81d
Justin Handville <nanolith@gmail.com>
2023-12-06 22:49:37 -0500

    message_subscribe_init_for_nl_ws_filter impl.

---
+++ src/message/message_subscribe_init_for_nl_ws_filter.c
@@ -0,0 +1,34 @@
+/**
+ * \file src/message/message_subscribe_init_for_nl_ws_filter.c
+ *
+ * \brief \ref message_subscribe type init method for newline preserving
+ * whitespace filter subscriptions.
+ *
+ * \copyright 2023 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <libcparse/message/subscription.h>
+
+CPARSE_IMPORT_message_subscription;
+
+/**
+ * \brief Initialize a \ref message_subscribe instance for subscribing to the
+ * newline preserving whitespace filter.
+ *
+ * \param msg               The message to initialize.
+ * \param handler           The \ref event_handler to add to this endpoint.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(message_subscribe_init_for_nl_ws_filter)(
+    CPARSE_SYM(message_subscribe)* msg, CPARSE_SYM(event_handler)* handler)
+{
+    return
+        message_subscribe_init(
+            msg,
+            CPARSE_MESSAGE_TYPE_NEWLINE_PRESERVING_WHITESPACE_FILTER_SUBSCRIBE,
+            handler);
+}
