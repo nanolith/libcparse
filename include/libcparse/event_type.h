36f99e1260aa3a1dfda078fc899bc5b8aec54d52
Justin Handville <nanolith@gmail.com>
2023-12-14 23:01:42 -0500

    Added minus token event type.

--- include/libcparse/event_type.h
+++ include/libcparse/event_type.h
@@ -70,6 +70,7 @@ enum CPARSE_SYM(event_type)
     CPARSE_EVENT_TYPE_TOKEN_DOT =                   0x007A,
     CPARSE_EVENT_TYPE_TOKEN_ARROW =                 0x007B,
     CPARSE_EVENT_TYPE_TOKEN_PLUS =                  0x007C,
+    CPARSE_EVENT_TYPE_TOKEN_MINUS =                 0x007D,
 
     CPARSE_EVENT_TYPE_UNKNOWN =                     0xFFFF,
 };
