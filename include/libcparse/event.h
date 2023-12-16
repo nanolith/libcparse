ccd796266840df0217da6c873f7af99e2acf20cc
Justin Handville <nanolith@gmail.com>
2023-12-16 00:33:17 -0500

    event_init_for_token_left_brace decl.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -153,6 +153,20 @@ int CPARSE_SYM(event_init_for_token_right_paren)(
     CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
 
 /**
+ * \brief Perform an in-place initialization of a left brace token event
+ * instance.
+ *
+ * \param ev                    Pointer to the event to initialize.
+ * \param cursor                The event cursor.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(event_init_for_token_left_brace)(
+    CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
+
+/**
  * \brief Perform an in-place disposal of an event instance.
  *
  * \param ev                    Pointer to the event to dispose.
