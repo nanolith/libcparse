4acefc9e7bd5de56fbdfd7780a9dd20f0a90d33e
Justin Handville <nanolith@gmail.com>
2023-12-16 02:53:47 -0500

    event_init_for_token_logical_and export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -463,6 +463,9 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
     static inline int sym ## event_init_for_token_percent( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_token_percent)(x,y); } \
+    static inline int sym ## event_init_for_token_logical_and( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return CPARSE_SYM(event_init_for_token_logical_and)(x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
