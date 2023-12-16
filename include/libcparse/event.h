c035aabf5f01ebb2bac76d03a8ed1ed9f62bdae4
Justin Handville <nanolith@gmail.com>
2023-12-16 02:47:20 -0500

    event_init_for_token_star export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -413,6 +413,9 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
     static inline int sym ## event_init_for_token_minus( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_token_minus)(x,y); } \
+    static inline int sym ## event_init_for_token_star( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return CPARSE_SYM(event_init_for_token_star)(x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
