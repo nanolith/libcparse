752f53f822c5fb41b4a0cff64b171d6a110c3d7e
Justin Handville <nanolith@gmail.com>
2023-12-16 03:02:14 -0500

    event_init_for_token_caret export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -529,6 +529,9 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
     static inline int sym ## event_init_for_token_pipe( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_token_pipe)(x,y); } \
+    static inline int sym ## event_init_for_token_caret( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return CPARSE_SYM(event_init_for_token_caret)(x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
