55d99143545df224bb476c4d339c42c6e4f48131
Justin Handville <nanolith@gmail.com>
2023-12-17 14:42:33 -0500

    event_init_for_token_mod_equal export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -697,6 +697,9 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
     static inline int sym ## event_init_for_token_div_equal( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_token_div_equal)(x,y); } \
+    static inline int sym ## event_init_for_token_mod_equal( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return CPARSE_SYM(event_init_for_token_mod_equal)(x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
