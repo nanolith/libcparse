ca554893d60d7f26f92a66328646d57036e744b1
Justin Handville <nanolith@gmail.com>
2023-12-17 16:25:18 -0500

    event_init_for_token_bitshift_right decl.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -645,6 +645,20 @@ int CPARSE_SYM(event_init_for_token_bitshift_left)(
     CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
 
 /**
+ * \brief Perform an in-place initialization of a bitshift right token event
+ * instance.
+ *
+ * \param ev                    Pointer to the event to initialize.
+ * \param cursor                The event cursor.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(event_init_for_token_bitshift_right)(
+    CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
+
+/**
  * \brief Perform an in-place disposal of an event instance.
  *
  * \param ev                    Pointer to the event to dispose.
