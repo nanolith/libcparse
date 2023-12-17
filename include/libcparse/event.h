1eefa7c55f6e46c8800a8b67ff7358b3f61b81e7
Justin Handville <nanolith@gmail.com>
2023-12-17 16:21:57 -0500

    event_init_for_token_right_shift_equal export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -799,6 +799,9 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
     static inline int sym ## event_init_for_token_left_shift_equal( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_token_left_shift_equal)(x,y); } \
+    static inline int sym ## event_init_for_token_right_shift_equal( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return CPARSE_SYM(event_init_for_token_right_shift_equal)(x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
