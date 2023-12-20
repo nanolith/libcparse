62531b0c469ead0000ed9ccb94aa35be8be8a6cf
Justin Handville <nanolith@gmail.com>
2023-12-19 22:17:30 -0500

    event_init_for_token_preprocessor_id_include export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -1026,6 +1026,11 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return \
                 CPARSE_SYM(event_init_for_token_preprocessor_id_endif)(x,y); } \
+    static inline int sym ## event_init_for_token_preprocessor_id_include( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return \
+                CPARSE_SYM(event_init_for_token_preprocessor_id_include)( \
+                    x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
