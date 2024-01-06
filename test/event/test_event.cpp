ba36acc490c72905a9219f6c08ad4ed1f762ab59
Justin Handville <nanolith@gmail.com>
2024-01-05 23:06:39 -0500

    Added token ampersand unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -542,3 +542,24 @@ TEST(token_logical_or)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create a token ampersand event.
+ */
+TEST(token_ampersand)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(STATUS_SUCCESS == event_init_for_token_ampersand(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(CPARSE_EVENT_TYPE_TOKEN_AMPERSAND == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
