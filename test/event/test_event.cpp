9e815fabd55079e3ff130e6cd64171f874afaecc
Justin Handville <nanolith@gmail.com>
2024-01-06 22:20:47 -0500

    Added token equal assign event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -691,3 +691,26 @@ TEST(token_not_equal_compare)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create a token equal assign event.
+ */
+TEST(token_equal_assign)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_equal_assign(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_EQUAL_ASSIGN == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
