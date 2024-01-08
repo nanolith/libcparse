bf70caccc2dc224341177321c7f6cc6f82ed2a70
Justin Handville <nanolith@gmail.com>
2024-01-07 22:34:20 -0500

    Added token times equal event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -760,3 +760,26 @@ TEST(token_minus_equal)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create a token times equal event.
+ */
+TEST(token_times_equal)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_times_equal(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_TIMES_EQUAL == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
