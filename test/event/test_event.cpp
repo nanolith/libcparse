98feb9616ba181e555f7960480b1dd36e265ff30
Justin Handville <nanolith@gmail.com>
2024-01-08 23:32:56 -0500

    Added token compl equal event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -898,3 +898,26 @@ TEST(token_xor_equal)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create a token compl equal event.
+ */
+TEST(token_compl_equal)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_compl_equal(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_COMPL_EQUAL == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
