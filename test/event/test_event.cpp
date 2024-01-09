1405095fb0e48cf057ca8e95e2cb7834e87d9b12
Justin Handville <nanolith@gmail.com>
2024-01-08 23:38:01 -0500

    Added token right shift equal event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -944,3 +944,26 @@ TEST(token_left_shift_equal)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create a token right shift equal event.
+ */
+TEST(token_right_shift_equal)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_right_shift_equal(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_RIGHT_SHIFT_EQUAL == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
