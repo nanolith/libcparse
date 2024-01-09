35d610b2cdd4906f4ab67d2d40719e7237d02237
Justin Handville <nanolith@gmail.com>
2024-01-08 23:30:42 -0500

    Added token xor equal event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -875,3 +875,26 @@ TEST(token_or_equal)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create a token xor equal event.
+ */
+TEST(token_xor_equal)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_xor_equal(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_XOR_EQUAL == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
