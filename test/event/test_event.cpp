b2c4b998218812b81c52337f21ae4640cf392b0d
Justin Handville <nanolith@gmail.com>
2024-01-09 20:12:43 -0500

    Added token less than event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -1013,3 +1013,26 @@ TEST(token_bitshift_right)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create a token less than event.
+ */
+TEST(token_less_than)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_less_than(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_LESS_THAN == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
