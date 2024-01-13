158846207902677c93a3ddd2a10418e7a7886005
Justin Handville <nanolith@gmail.com>
2024-01-12 22:25:18 -0500

    Added internal string include to stub.

--- src/event/stub.c
+++ src/event/stub.c
@@ -3,6 +3,8 @@
 #include <libcparse/event/string.h>
 #include <stdio.h>
 
+#include "event_string_internal.h"
+
 CPARSE_IMPORT_event_include;
 CPARSE_IMPORT_event_raw_line;
 CPARSE_IMPORT_event_string;
