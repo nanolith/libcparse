566e45639902a0179810d4b216d3faf0ef4c7847
Justin Handville <nanolith@gmail.com>
2024-01-13 21:15:17 -0500

    event_string_init unit test.

---
+++ test/event/test_event_string.cpp
@@ -0,0 +1,46 @@
+/**
+ * \file test/event/test_event_string.cpp
+ *
+ * \brief Tests for the \ref event_string type.
+ *
+ * \copyright 2023 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <libcparse/cursor.h>
+#include <libcparse/event.h>
+#include <libcparse/event/string.h>
+#include <libcparse/status_codes.h>
+#include <minunit/minunit.h>
+#include <string.h>
+
+CPARSE_IMPORT_cursor;
+CPARSE_IMPORT_event;
+CPARSE_IMPORT_event_string;
+
+TEST_SUITE(event_string);
+
+/**
+ * Test that we can create a string value event.
+ */
+TEST(event_string_init)
+{
+    event_string ev;
+    cursor c;
+    const char* TEST_STRING = "test";
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(STATUS_SUCCESS == event_string_init(&ev, &c, TEST_STRING));
+
+    /* get the base event type. */
+    auto bev = event_string_upcast(&ev);
+
+    /* The event type is correct. */
+    TEST_EXPECT(CPARSE_EVENT_TYPE_TOKEN_VALUE_STRING == event_get_type(bev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_string_dispose(&ev));
+}
