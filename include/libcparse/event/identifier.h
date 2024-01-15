25123311f0e0f47d3fa0a1c9b43f1836f062942b
Justin Handville <nanolith@gmail.com>
2024-01-14 23:05:49 -0500

    event_downcast_to_event_identifier decl.

--- include/libcparse/event/identifier.h
+++ include/libcparse/event/identifier.h
@@ -69,6 +69,21 @@ int CPARSE_SYM(event_identifier_dispose)(CPARSE_SYM(event_identifier)* ev);
 const char* CPARSE_SYM(event_identifier_get)(
     const CPARSE_SYM(event_identifier)* ev);
 
+/**
+ * \brief Attempt to downcast an \ref event to an \ref event_identifier.
+ *
+ * \param i_ev              Pointer to the event pointer to receive the downcast
+ *                          instance on success.
+ * \param ev                The \ref event pointer to attempt to downcast to the
+ *                          derived type.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(event_downcast_to_event_identifier)(
+    CPARSE_SYM(event_identifier)** i_ev, CPARSE_SYM(event)* ev);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
