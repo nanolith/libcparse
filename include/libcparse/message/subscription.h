c6ebca2103dc594b5edf322b1e62b088567ff88d
Justin Handville <nanolith@gmail.com>
2024-01-19 23:28:21 -0500

    message_subscribe_init_for_preprocessor_scanner export.

--- include/libcparse/message/subscription.h
+++ include/libcparse/message/subscription.h
@@ -203,6 +203,11 @@ CPARSE_SYM(message)* CPARSE_SYM(message_subscribe_upcast)(
         CPARSE_SYM(message_subscribe)* x, CPARSE_SYM(event_handler)* y) { \
             return \
                 CPARSE_SYM(message_subscribe_init_for_nl_ws_filter)(x,y); } \
+    static inline int sym ## message_subscribe_init_for_preprocessor_scanner(\
+        CPARSE_SYM(message_subscribe)* x, CPARSE_SYM(event_handler)* y) { \
+            return \
+                CPARSE_SYM(message_subscribe_init_for_preprocessor_scanner)( \
+                    x,y); } \
     static inline int sym ## message_subscribe_init_for_rflo_filter(\
         CPARSE_SYM(message_subscribe)* x, CPARSE_SYM(event_handler)* y) { \
             return \
