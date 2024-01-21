6ea6fbdf9a7b131a0a327a53286c79645412b9a4
Justin Handville <nanolith@gmail.com>
2024-01-20 21:58:56 -0500

    Added basic EOF support.

--- src/preprocessor_scanner/preprocessor_scanner_event_callback.c
+++ src/preprocessor_scanner/preprocessor_scanner_event_callback.c
@@ -7,9 +7,19 @@
  * distribution for the license terms under which this software is distributed.
  */
 
+#include <libcparse/event.h>
+#include <libcparse/event_reactor.h>
+#include <libcparse/preprocessor_scanner.h>
 #include <libcparse/status_codes.h>
 
 #include "preprocessor_scanner_internal.h"
+
+CPARSE_IMPORT_event;
+CPARSE_IMPORT_event_reactor;
+CPARSE_IMPORT_preprocessor_scanner;
+
+static int process_eof_event(
+    preprocessor_scanner* scanner, const event* ev);
 
 /**
  * \brief Event handler callback for \ref preprocessor_scanner_event_callback.
@@ -25,9 +35,34 @@
 int CPARSE_SYM(preprocessor_scanner_event_callback)(
     void* context, const CPARSE_SYM(event)* ev)
 {
-    /* TODO - fill out stub. */
-    (void)context;
-    (void)ev;
+    preprocessor_scanner* scanner = (preprocessor_scanner*)context;
 
-    return STATUS_SUCCESS;
+    switch (event_get_type(ev))
+    {
+        case CPARSE_EVENT_TYPE_EOF:
+            return process_eof_event(scanner, ev);
+
+        default:
+            return STATUS_SUCCESS;
+    }
+}
+
+/**
+ * \brief Process an eof event.
+ *
+ * \param scanner           The scanner for this operation.
+ * \param ev                The raw character event to process.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int process_eof_event(
+    preprocessor_scanner* scanner, const event* ev)
+{
+    switch (scanner->state)
+    {
+        default:
+            return event_reactor_broadcast(scanner->reactor, ev);
+    }
 }
