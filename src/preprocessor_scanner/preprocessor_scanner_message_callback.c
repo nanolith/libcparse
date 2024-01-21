de4748b9d3c246bde305f661a62825e14553fcf6
Justin Handville <nanolith@gmail.com>
2024-01-20 21:55:27 -0500

    Handle subscriptions to the preprocessor scanner.

--- src/preprocessor_scanner/preprocessor_scanner_message_callback.c
+++ src/preprocessor_scanner/preprocessor_scanner_message_callback.c
@@ -7,16 +7,24 @@
  * distribution for the license terms under which this software is distributed.
  */
 
+#include <libcparse/event_handler.h>
+#include <libcparse/event_reactor.h>
 #include <libcparse/message.h>
+#include <libcparse/message/subscription.h>
 #include <libcparse/message_handler.h>
 #include <libcparse/preprocessor_scanner.h>
 #include <libcparse/status_codes.h>
 
 #include "preprocessor_scanner_internal.h"
 
+CPARSE_IMPORT_event_handler;
+CPARSE_IMPORT_event_reactor;
 CPARSE_IMPORT_message;
 CPARSE_IMPORT_message_handler;
+CPARSE_IMPORT_message_subscription;
 CPARSE_IMPORT_preprocessor_scanner;
+
+static int subscribe(preprocessor_scanner* scanner, const message* msg);
 
 /**
  * \brief Message handler callback for
@@ -37,7 +45,51 @@ int CPARSE_SYM(preprocessor_scanner_message_callback)(
 
     switch (message_get_type(msg))
     {
+        case CPARSE_MESSAGE_TYPE_PREPROCESSOR_SCANNER_SUBSCRIBE:
+            return subscribe(scanner, msg);
+
         default:
             return message_handler_send(&scanner->parent_mh, msg);
     }
+}
+
+/**
+ * \brief Subscribe to the preprocessor_scanner.
+ *
+ * \param scanner           The scanner for this operation.
+ * \param msg               The message for this operation.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int subscribe(preprocessor_scanner* scanner, const message* msg)
+{
+    int retval;
+    message_subscribe* m;
+    const event_handler* eh;
+
+    /* dynamic cast the message. */
+    retval = message_downcast_to_message_subscribe(&m, (message*)msg);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* get the event handler for this message. */
+    eh = message_subscribe_event_handler_get(m);
+
+    /* add this handler to our reactor. */
+    retval = event_reactor_add(scanner->reactor, eh);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* success. */
+    retval = STATUS_SUCCESS;
+    goto done;
+
+done:
+    return retval;
 }
