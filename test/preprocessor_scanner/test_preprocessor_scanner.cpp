a5feb7d2a89499ef4a1aad376d01ef7f7655e4d1
Justin Handville <nanolith@gmail.com>
2024-01-21 21:25:31 -0500

    processor_scanner identifier newline unit test.

--- test/preprocessor_scanner/test_preprocessor_scanner.cpp
+++ test/preprocessor_scanner/test_preprocessor_scanner.cpp
@@ -281,3 +281,88 @@ TEST(identifier2)
         STATUS_SUCCESS == preprocessor_scanner_release(scanner));
     TEST_ASSERT(STATUS_SUCCESS == event_handler_dispose(&eh));
 }
+
+/**
+ * Test that we can scan multiple identifiers separated by newlines.
+ */
+TEST(identifier3)
+{
+    preprocessor_scanner* scanner;
+    input_stream* stream;
+    event_handler eh;
+    test_context t1;
+    const char* INPUT_STRING = "abc123_\n_12\nx";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == preprocessor_scanner_create(&scanner));
+
+    /* create an event handler. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_handler_init(&eh, &dummy_callback, &t1));
+
+    /* get the abstract parser. */
+    auto ap = preprocessor_scanner_upcast(scanner);
+
+    /* subscribe to the scanner. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == abstract_parser_preprocessor_scanner_subscribe(ap, &eh));
+
+    /* create an input stream. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == input_stream_create_from_string(&stream, INPUT_STRING));
+
+    /* add the input stream to the parser. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == abstract_parser_push_input_stream(ap, "stdin", stream));
+
+    /* precondition: eof is false. */
+    TEST_ASSERT(!t1.eof);
+
+    /* precondition: vals is empty. */
+    TEST_ASSERT(t1.vals.empty());
+
+    /* run the filter. */
+    TEST_ASSERT(STATUS_SUCCESS == abstract_parser_run(ap));
+
+    /* postcondition: eof is true. */
+    TEST_EXPECT(t1.eof);
+
+    /* postcondition: there are three values in vals. */
+    TEST_ASSERT(3 == t1.vals.size());
+
+    /* get the first value. */
+    auto f = t1.vals.begin();
+    TEST_ASSERT(f != t1.vals.end());
+
+    /* this first value is an identifier. */
+    TEST_EXPECT(CPARSE_EVENT_TYPE_TOKEN_IDENTIFIER == f->first);
+    TEST_EXPECT("abc123_" == f->second);
+
+    /* get the second value. */
+    ++f;
+    TEST_ASSERT(f != t1.vals.end());
+
+    /* this second value is an identifier. */
+    TEST_EXPECT(CPARSE_EVENT_TYPE_TOKEN_IDENTIFIER == f->first);
+    TEST_EXPECT("_12" == f->second);
+
+    /* get the third value. */
+    ++f;
+    TEST_ASSERT(f != t1.vals.end());
+
+    /* this third value is an identifier. */
+    TEST_EXPECT(CPARSE_EVENT_TYPE_TOKEN_IDENTIFIER == f->first);
+    TEST_EXPECT("x" == f->second);
+
+    /* there are no more values. */
+    TEST_EXPECT(++f == t1.vals.end());
+
+    /* clean up. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == preprocessor_scanner_release(scanner));
+    TEST_ASSERT(STATUS_SUCCESS == event_handler_dispose(&eh));
+}
