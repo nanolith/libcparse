b0856b86517e958fb49d8d4b8935060c83627a80
Justin Handville <nanolith@gmail.com>
2024-01-24 20:31:31 -0500

    Added plus token event.

--- src/preprocessor_scanner/preprocessor_scanner_event_callback.c
+++ src/preprocessor_scanner/preprocessor_scanner_event_callback.c
@@ -70,6 +70,8 @@ static int broadcast_dot_token(
     preprocessor_scanner* scanner, const event* ev);
 static int broadcast_arrow_token(
     preprocessor_scanner* scanner, const event* ev);
+static int broadcast_plus_token(
+    preprocessor_scanner* scanner, const event* ev);
 
 /**
  * \brief Event handler callback for \ref preprocessor_scanner_event_callback.
@@ -245,6 +247,9 @@ static int process_raw_character(
 
                     case '-':
                         return start_dash(scanner, ev);
+
+                    case '+':
+                        return broadcast_plus_token(scanner, ev);
 
                     default:
                         return
@@ -1046,6 +1051,57 @@ static int broadcast_arrow_token(
 
     /* clear the file / position cache. */
     file_position_cache_clear(scanner->cache);
+
+    /* we are now in the init state. */
+    scanner->state = CPARSE_PREPROCESSOR_SCANNER_STATE_INIT;
+
+    /* success. */
+    goto cleanup_tev;
+
+cleanup_tev:
+    release_retval = event_dispose(&tev);
+    if (STATUS_SUCCESS != release_retval)
+    {
+        retval = release_retval;
+    }
+
+done:
+    return retval;
+}
+
+/**
+ * \brief Broadcast a plus token.
+ *
+ * \param scanner           The scanner for this operation.
+ * \param ev                The raw character event for this operation.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int broadcast_plus_token(
+    preprocessor_scanner* scanner, const event* ev)
+{
+    int retval, release_retval;
+    const cursor* pos;
+    event tev;
+
+    /* get the event position. */
+    pos = event_get_cursor(ev);
+
+    /* initialize the token event. */
+    retval = event_init_for_token_plus(&tev, pos);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* broadcast this event. */
+    retval = event_reactor_broadcast(scanner->reactor, &tev);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto cleanup_tev;
+    }
 
     /* we are now in the init state. */
     scanner->state = CPARSE_PREPROCESSOR_SCANNER_STATE_INIT;
