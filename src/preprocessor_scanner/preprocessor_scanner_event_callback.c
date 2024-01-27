f7756447409d36edb2bf08fc7d568f90c4ffd75f
Justin Handville <nanolith@gmail.com>
2024-01-26 22:48:12 -0500

    Added percent token support.

--- src/preprocessor_scanner/preprocessor_scanner_event_callback.c
+++ src/preprocessor_scanner/preprocessor_scanner_event_callback.c
@@ -51,6 +51,7 @@ static int start_dash(preprocessor_scanner* scanner, const event* ev);
 static int start_plus(preprocessor_scanner* scanner, const event* ev);
 static int start_star(preprocessor_scanner* scanner, const event* ev);
 static int start_slash(preprocessor_scanner* scanner, const event* ev);
+static int start_percent(preprocessor_scanner* scanner, const event* ev);
 static int broadcast_left_paren_token(
     preprocessor_scanner* scanner, const event* ev);
 static int broadcast_right_paren_token(
@@ -80,6 +81,8 @@ static int broadcast_minus_token(
 static int broadcast_star_token(
     preprocessor_scanner* scanner, const event* ev);
 static int broadcast_slash_token(
+    preprocessor_scanner* scanner, const event* ev);
+static int broadcast_percent_token(
     preprocessor_scanner* scanner, const event* ev);
 
 /**
@@ -147,6 +150,9 @@ static int process_eof_event(
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_SLASH:
             return broadcast_slash_token(scanner, ev);
 
+        case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_PERCENT:
+            return broadcast_percent_token(scanner, ev);
+
         default:
             return event_reactor_broadcast(scanner->reactor, ev);
     }
@@ -182,6 +188,9 @@ static int process_whitespace_event(
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_SLASH:
             return broadcast_slash_token(scanner, ev);
 
+        case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_PERCENT:
+            return broadcast_percent_token(scanner, ev);
+
         default:
             return STATUS_SUCCESS;
     }
@@ -216,6 +225,9 @@ static int process_newline_event(
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_SLASH:
             return broadcast_slash_token(scanner, ev);
+
+        case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_PERCENT:
+            return broadcast_percent_token(scanner, ev);
 
         default:
             return STATUS_SUCCESS;
@@ -302,6 +314,9 @@ static int process_raw_character(
                     case '/':
                         return start_slash(scanner, ev);
 
+                    case '%':
+                        return start_percent(scanner, ev);
+
                     default:
                         return
                             ERROR_LIBCPARSE_PP_SCANNER_UNEXPECTED_CHARACTER;
@@ -339,6 +354,13 @@ static int process_raw_character(
             {
                 default:
                     return broadcast_slash_token(scanner, ev);
+            }
+
+        case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_PERCENT:
+            switch (ch)
+            {
+                default:
+                    return broadcast_percent_token(scanner, ev);
             }
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_IDENTIFIER:
@@ -1169,6 +1191,37 @@ static int start_slash(preprocessor_scanner* scanner, const event* ev)
 }
 
 /**
+ * \brief Start the percent state.
+ *
+ * \param scanner           The scanner for this operation.
+ * \param ev                The raw character event to process.
+ * \param ch                The character for this identifier.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int start_percent(preprocessor_scanner* scanner, const event* ev)
+{
+    int retval;
+
+    /* get the cursor for this event. */
+    const cursor* pos = event_get_cursor(ev);
+
+    /* cache the location for the start of this event. */
+    retval = file_position_cache_set(scanner->cache, pos->file, pos);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* we are now in the percent state. */
+    scanner->state = CPARSE_PREPROCESSOR_SCANNER_STATE_IN_PERCENT;
+
+    return STATUS_SUCCESS;
+}
+
+/**
  * \brief Broadcast an arrow token.
  *
  * \param scanner           The scanner for this operation.
@@ -1451,6 +1504,70 @@ static int broadcast_slash_token(
 
     /* initialize the token event. */
     retval = event_init_for_token_forward_slash(&tev, pos);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* broadcast this event. */
+    retval = event_reactor_broadcast(scanner->reactor, &tev);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto cleanup_tev;
+    }
+
+    /* clear the file / position cache. */
+    file_position_cache_clear(scanner->cache);
+
+    /* we are now in the init state. */
+    scanner->state = CPARSE_PREPROCESSOR_SCANNER_STATE_INIT;
+
+    /* success. */
+    goto cleanup_tev;
+
+cleanup_tev:
+    release_retval = event_dispose(&tev);
+    if (STATUS_SUCCESS != release_retval)
+    {
+        retval = release_retval;
+    }
+
+done:
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* if we succeed, then recursively process the new event on the way out. */
+    return preprocessor_scanner_event_callback(scanner, ev);
+}
+
+/**
+ * \brief Broadcast a percent token.
+ *
+ * \param scanner           The scanner for this operation.
+ * \param ev                The event to process AFTER this token.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int broadcast_percent_token(
+    preprocessor_scanner* scanner, const event* ev)
+{
+    int retval, release_retval;
+    const cursor* pos;
+    event tev;
+
+    /* get the cached position. */
+    retval = file_position_cache_position_get(scanner->cache, &pos);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* initialize the token event. */
+    retval = event_init_for_token_percent(&tev, pos);
     if (STATUS_SUCCESS != retval)
     {
         goto done;
