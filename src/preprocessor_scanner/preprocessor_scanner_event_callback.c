715fb2bbb2b43089e39d21c72b1ffb884ab7a0a4
Justin Handville <nanolith@gmail.com>
2024-01-27 21:52:53 -0500

    Added pipe token support.

--- src/preprocessor_scanner/preprocessor_scanner_event_callback.c
+++ src/preprocessor_scanner/preprocessor_scanner_event_callback.c
@@ -92,6 +92,8 @@ static int broadcast_logical_or_token(
     preprocessor_scanner* scanner, const event* ev);
 static int broadcast_ampersand_token(
     preprocessor_scanner* scanner, const event* ev);
+static int broadcast_pipe_token(
+    preprocessor_scanner* scanner, const event* ev);
 
 /**
  * \brief Event handler callback for \ref preprocessor_scanner_event_callback.
@@ -165,7 +167,7 @@ static int process_eof_event(
             return broadcast_ampersand_token(scanner, ev);;
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_PIPE:
-            return ERROR_LIBCPARSE_PP_SCANNER_BAD_STATE;
+            return broadcast_pipe_token(scanner, ev);
 
         default:
             return event_reactor_broadcast(scanner->reactor, ev);
@@ -209,7 +211,7 @@ static int process_whitespace_event(
             return broadcast_ampersand_token(scanner, ev);
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_PIPE:
-            return ERROR_LIBCPARSE_PP_SCANNER_BAD_STATE;
+            return broadcast_pipe_token(scanner, ev);
 
         default:
             return STATUS_SUCCESS;
@@ -253,7 +255,7 @@ static int process_newline_event(
             return broadcast_ampersand_token(scanner, ev);
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_PIPE:
-            return ERROR_LIBCPARSE_PP_SCANNER_BAD_STATE;
+            return broadcast_pipe_token(scanner, ev);
 
         default:
             return STATUS_SUCCESS;
@@ -412,7 +414,7 @@ static int process_raw_character(
                     return broadcast_logical_or_token(scanner, ev);
 
                 default:
-                    return ERROR_LIBCPARSE_PP_SCANNER_UNEXPECTED_CHARACTER;
+                    return broadcast_pipe_token(scanner, ev);
             }
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_IDENTIFIER:
@@ -1876,6 +1878,70 @@ static int broadcast_ampersand_token(
 
     /* initialize the token event. */
     retval = event_init_for_token_ampersand(&tev, pos);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* broadcast this event. */
+    retval = event_reactor_broadcast(scanner->reactor, &tev);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto cleanup_tev;
+    }
+
+    /* clear the file / position cache. */
+    file_position_cache_clear(scanner->cache);
+
+    /* we are now in the init state. */
+    scanner->state = CPARSE_PREPROCESSOR_SCANNER_STATE_INIT;
+
+    /* success. */
+    goto cleanup_tev;
+
+cleanup_tev:
+    release_retval = event_dispose(&tev);
+    if (STATUS_SUCCESS != release_retval)
+    {
+        retval = release_retval;
+    }
+
+done:
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* if we succeed, then recursively process the new event on the way out. */
+    return preprocessor_scanner_event_callback(scanner, ev);
+}
+
+/**
+ * \brief Broadcast a pipe token.
+ *
+ * \param scanner           The scanner for this operation.
+ * \param ev                The event to process AFTER this token.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int broadcast_pipe_token(
+    preprocessor_scanner* scanner, const event* ev)
+{
+    int retval, release_retval;
+    const cursor* pos;
+    event tev;
+
+    /* get the cached position. */
+    retval = file_position_cache_position_get(scanner->cache, &pos);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* initialize the token event. */
+    retval = event_init_for_token_pipe(&tev, pos);
     if (STATUS_SUCCESS != retval)
     {
         goto done;
