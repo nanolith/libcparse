d7338fa411406c7ea889ba90df2b7f429d77b3ba
Justin Handville <nanolith@gmail.com>
2024-01-28 22:08:33 -0500

    event_init_for_token_not export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -1147,6 +1147,9 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
     static inline int sym ## event_init_for_token_decrement( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_token_decrement)(x,y); } \
+    static inline int sym ## event_init_for_token_not( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return CPARSE_SYM(event_init_for_token_not)(x,y); } \
     static inline int sym ## event_init_for_token_preprocessor_id_if( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_token_preprocessor_id_if)(x,y); } \
