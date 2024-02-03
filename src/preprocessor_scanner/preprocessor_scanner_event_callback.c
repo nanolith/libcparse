1588ac6453b3de9cd9e00bff6b4e6378b3fc6dad
Justin Handville <nanolith@gmail.com>
2024-02-02 22:57:16 -0500

    Added greater than equal token support.

--- src/preprocessor_scanner/preprocessor_scanner_event_callback.c
+++ src/preprocessor_scanner/preprocessor_scanner_event_callback.c
@@ -146,6 +146,8 @@ static int broadcast_greater_than_token(
     preprocessor_scanner* scanner, const event* ev);
 static int broadcast_less_than_equal_token(
     preprocessor_scanner* scanner, const event* ev);
+static int broadcast_greater_than_equal_token(
+    preprocessor_scanner* scanner, const event* ev);
 
 /**
  * \brief Event handler callback for \ref preprocessor_scanner_event_callback.
@@ -651,6 +653,9 @@ static int process_raw_character(
             {
                 case '>':
                     return start_gt_gt(scanner, ev);
+
+                case '=':
+                    return broadcast_greater_than_equal_token(scanner, ev);
 
                 default:
                     return broadcast_greater_than_token(scanner, ev);
@@ -3848,6 +3853,71 @@ static int broadcast_less_than_equal_token(
 
     /* initialize the token event. */
     retval = event_init_for_token_less_than_equal(&tev, pos);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* broadcast this event. */
+    retval = event_reactor_broadcast(scanner->reactor, &tev);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto cleanup_tev;
+    }
+
+    /* clear the file / position cache. */
+    file_position_cache_clear(scanner->cache);
+
+    /* we are now in the init state. */
+    scanner->state = CPARSE_PREPROCESSOR_SCANNER_STATE_INIT;
+
+    /* success. */
+    goto cleanup_tev;
+
+cleanup_tev:
+    release_retval = event_dispose(&tev);
+    if (STATUS_SUCCESS != release_retval)
+    {
+        retval = release_retval;
+    }
+
+done:
+    return retval;
+}
+
+/**
+ * \brief Broadcast a greater than equal token.
+ *
+ * \param scanner           The scanner for this operation.
+ * \param ev                The raw character event for this operation.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int broadcast_greater_than_equal_token(
+    preprocessor_scanner* scanner, const event* ev)
+{
+    int retval, release_retval;
+    const cursor* pos;
+    event tev;
+
+    /* extend the cached position to cover this character. */
+    retval = file_position_cache_position_extend(scanner->cache, ev);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* get the cached position. */
+    retval = file_position_cache_position_get(scanner->cache, &pos);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* initialize the token event. */
+    retval = event_init_for_token_greater_than_equal(&tev, pos);
     if (STATUS_SUCCESS != retval)
     {
         goto done;
