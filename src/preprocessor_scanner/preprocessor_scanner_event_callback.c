a18dddab67a6acd76b26882083d9fec6c2dedfc8
Justin Handville <nanolith@gmail.com>
2024-02-08 21:54:42 -0500

    DRY not equal compare token broadcast.

--- src/preprocessor_scanner/preprocessor_scanner_event_callback.c
+++ src/preprocessor_scanner/preprocessor_scanner_event_callback.c
@@ -59,8 +59,6 @@ static int broadcast_compound_token(
     preprocessor_scanner* scanner, const event* ev, simple_event_ctor ctor);
 static int broadcast_cached_token_and_continue(
     preprocessor_scanner* scanner, const event* ev, simple_event_ctor ctor);
-static int broadcast_not_equal_compare_token(
-    preprocessor_scanner* scanner, const event* ev);
 static int broadcast_bitshift_left_equal_token(
     preprocessor_scanner* scanner, const event* ev);
 static int broadcast_bitshift_right_equal_token(
@@ -729,7 +727,10 @@ static int process_raw_character(
             switch (ch)
             {
                 case '=':
-                    return broadcast_not_equal_compare_token(scanner, ev);
+                    return
+                        broadcast_compound_token(
+                            scanner, ev,
+                            &event_init_for_token_not_equal_compare);
 
                 default:
                     return broadcast_not_token(scanner, ev);
@@ -1060,71 +1061,6 @@ static int start_gt_gt(preprocessor_scanner* scanner, const event* ev)
     scanner->state = CPARSE_PREPROCESSOR_SCANNER_STATE_IN_GT_GT;
 
     return STATUS_SUCCESS;
-}
-
-/**
- * \brief Broadcast a not equal compare token.
- *
- * \param scanner           The scanner for this operation.
- * \param ev                The raw character event for this operation.
- *
- * \returns a status code indicating success or failure.
- *      - STATUS_SUCCESS on success.
- *      - a non-zero error code on failure.
- */
-static int broadcast_not_equal_compare_token(
-    preprocessor_scanner* scanner, const event* ev)
-{
-    int retval, release_retval;
-    const cursor* pos;
-    event tev;
-
-    /* extend the cached position to cover this character. */
-    retval = file_position_cache_position_extend(scanner->cache, ev);
-    if (STATUS_SUCCESS != retval)
-    {
-        return retval;
-    }
-
-    /* get the cached position. */
-    retval = file_position_cache_position_get(scanner->cache, &pos);
-    if (STATUS_SUCCESS != retval)
-    {
-        goto done;
-    }
-
-    /* initialize the token event. */
-    retval = event_init_for_token_not_equal_compare(&tev, pos);
-    if (STATUS_SUCCESS != retval)
-    {
-        goto done;
-    }
-
-    /* broadcast this event. */
-    retval = event_reactor_broadcast(scanner->reactor, &tev);
-    if (STATUS_SUCCESS != retval)
-    {
-        goto cleanup_tev;
-    }
-
-    /* clear the file / position cache. */
-    file_position_cache_clear(scanner->cache);
-
-    /* we are now in the init state. */
-    scanner->state = CPARSE_PREPROCESSOR_SCANNER_STATE_INIT;
-
-    /* success. */
-    goto cleanup_tev;
-
-cleanup_tev:
-    release_retval = event_dispose(&tev);
-    if (STATUS_SUCCESS != release_retval)
-    {
-        retval = release_retval;
-    }
-
-done:
-    return retval;
 }
 
 /**
