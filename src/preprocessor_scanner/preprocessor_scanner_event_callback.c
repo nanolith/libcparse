6cacf53b9391dafd84d2b2f4b0f0320c409b6b57
Justin Handville <nanolith@gmail.com>
2024-02-08 21:51:00 -0500

    DRY tilde token broadcast.

--- src/preprocessor_scanner/preprocessor_scanner_event_callback.c
+++ src/preprocessor_scanner/preprocessor_scanner_event_callback.c
@@ -59,8 +59,6 @@ static int broadcast_compound_token(
     preprocessor_scanner* scanner, const event* ev, simple_event_ctor ctor);
 static int broadcast_cached_token_and_continue(
     preprocessor_scanner* scanner, const event* ev, simple_event_ctor ctor);
-static int broadcast_tilde_token(
-    preprocessor_scanner* scanner, const event* ev);
 static int broadcast_equal_compare_token(
     preprocessor_scanner* scanner, const event* ev);
 static int broadcast_not_equal_compare_token(
@@ -180,7 +178,9 @@ static int process_eof_event(
                     scanner, ev, &event_init_for_token_caret);
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_TILDE:
-            return broadcast_tilde_token(scanner, ev);
+            return
+                broadcast_cached_token_and_continue(
+                    scanner, ev, &event_init_for_token_tilde);
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_EQUAL:
             return broadcast_equal_assign_token(scanner, ev);
@@ -264,7 +264,9 @@ static int process_whitespace_event(
                     scanner, ev, &event_init_for_token_caret);
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_TILDE:
-            return broadcast_tilde_token(scanner, ev);
+            return
+                broadcast_cached_token_and_continue(
+                    scanner, ev, &event_init_for_token_tilde);
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_EQUAL:
             return broadcast_equal_assign_token(scanner, ev);
@@ -348,7 +350,9 @@ static int process_newline_event(
                     scanner, ev, &event_init_for_token_caret);
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_TILDE:
-            return broadcast_tilde_token(scanner, ev);
+            return
+                broadcast_cached_token_and_continue(
+                    scanner, ev, &event_init_for_token_tilde);
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_EQUAL:
             return broadcast_equal_assign_token(scanner, ev);
@@ -700,7 +704,9 @@ static int process_raw_character(
                             scanner, ev, &event_init_for_token_compl_equal);
 
                 default:
-                    return broadcast_tilde_token(scanner, ev);
+                    return
+                        broadcast_cached_token_and_continue(
+                            scanner, ev, &event_init_for_token_tilde);
             }
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_EQUAL:
@@ -1048,70 +1054,6 @@ static int start_gt_gt(preprocessor_scanner* scanner, const event* ev)
     scanner->state = CPARSE_PREPROCESSOR_SCANNER_STATE_IN_GT_GT;
 
     return STATUS_SUCCESS;
-}
-
-/**
- * \brief Broadcast a tilde token.
- *
- * \param scanner           The scanner for this operation.
- * \param ev                The event to process AFTER this token.
- *
- * \returns a status code indicating success or failure.
- *      - STATUS_SUCCESS on success.
- *      - a non-zero error code on failure.
- */
-static int broadcast_tilde_token(
-    preprocessor_scanner* scanner, const event* ev)
-{
-    int retval, release_retval;
-    const cursor* pos;
-    event tev;
-
-    /* get the cached position. */
-    retval = file_position_cache_position_get(scanner->cache, &pos);
-    if (STATUS_SUCCESS != retval)
-    {
-        goto done;
-    }
-
-    /* initialize the token event. */
-    retval = event_init_for_token_tilde(&tev, pos);
-    if (STATUS_SUCCESS != retval)
-    {
-        goto done;
-    }
-
-    /* broadcast this event. */
-    retval = event_reactor_broadcast(scanner->reactor, &tev);
-    if (STATUS_SUCCESS != retval)
-    {
-        goto cleanup_tev;
-    }
-
-    /* clear the file / position cache. */
-    file_position_cache_clear(scanner->cache);
-
-    /* we are now in the init state. */
-    scanner->state = CPARSE_PREPROCESSOR_SCANNER_STATE_INIT;
-
-    /* success. */
-    goto cleanup_tev;
-
-cleanup_tev:
-    release_retval = event_dispose(&tev);
-    if (STATUS_SUCCESS != release_retval)
-    {
-        retval = release_retval;
-    }
-
-done:
-    if (STATUS_SUCCESS != retval)
-    {
-        return retval;
-    }
-
-    /* if we succeed, then recursively process the new event on the way out. */
-    return preprocessor_scanner_event_callback(scanner, ev);
 }
 
 /**
