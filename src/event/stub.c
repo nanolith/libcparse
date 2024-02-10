7f0f7650bf5ed33ca0fec09e338693597bc79c8a
Justin Handville <nanolith@gmail.com>
2024-02-09 21:42:28 -0500

    Added event/integer header to stub.

--- src/event/stub.c
+++ src/event/stub.c
@@ -1,5 +1,6 @@
 #include <libcparse/event/include.h>
 #include <libcparse/event/identifier.h>
+#include <libcparse/event/integer.h>
 #include <libcparse/event/raw_line.h>
 #include <libcparse/event/string.h>
 #include <stdio.h>
