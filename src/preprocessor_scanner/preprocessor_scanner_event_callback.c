cc92e18f6150791bee878f512aba9def4ed8a841
Justin Handville <nanolith@gmail.com>
2024-02-09 21:37:53 -0500

    DRY greater than token broadcast.

--- src/preprocessor_scanner/preprocessor_scanner_event_callback.c
+++ src/preprocessor_scanner/preprocessor_scanner_event_callback.c
@@ -63,8 +63,6 @@ static int broadcast_bitshift_right_equal_token(
     preprocessor_scanner* scanner, const event* ev);
 static int broadcast_bitshift_right_token(
     preprocessor_scanner* scanner, const event* ev);
-static int broadcast_greater_than_token(
-    preprocessor_scanner* scanner, const event* ev);
 
 /**
  * \brief Event handler callback for \ref preprocessor_scanner_event_callback.
@@ -185,7 +183,9 @@ static int process_eof_event(
                     scanner, ev, &event_init_for_token_bitshift_left);
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_GT:
-            return broadcast_greater_than_token(scanner, ev);
+            return
+                broadcast_cached_token_and_continue(
+                    scanner, ev, &event_init_for_token_greater_than);
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_GT_GT:
             return broadcast_bitshift_right_token(scanner, ev);
@@ -279,7 +279,9 @@ static int process_whitespace_event(
                     scanner, ev, &event_init_for_token_bitshift_left);
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_GT:
-            return broadcast_greater_than_token(scanner, ev);
+            return
+                broadcast_cached_token_and_continue(
+                    scanner, ev, &event_init_for_token_greater_than);
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_GT_GT:
             return broadcast_bitshift_right_token(scanner, ev);
@@ -373,7 +375,9 @@ static int process_newline_event(
                     scanner, ev, &event_init_for_token_bitshift_left);
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_GT:
-            return broadcast_greater_than_token(scanner, ev);
+            return
+                broadcast_cached_token_and_continue(
+                    scanner, ev, &event_init_for_token_greater_than);
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_GT_GT:
             return broadcast_bitshift_right_token(scanner, ev);
@@ -795,7 +799,9 @@ static int process_raw_character(
                             &event_init_for_token_greater_than_equal);
 
                 default:
-                    return broadcast_greater_than_token(scanner, ev);
+                    return
+                        broadcast_cached_token_and_continue(
+                            scanner, ev, &event_init_for_token_greater_than);
             }
 
         case CPARSE_PREPROCESSOR_SCANNER_STATE_IN_GT_GT:
@@ -1153,70 +1159,6 @@ static int broadcast_bitshift_right_token(
 
     /* initialize the token event. */
     retval = event_init_for_token_bitshift_right(&tev, pos);
-    if (STATUS_SUCCESS != retval)
-    {
-        goto done;
-    }
-
-    /* broadcast this event. */
-    retval = event_reactor_broadcast(scanner->reactor, &tev);
-    if (STATUS_SUCCESS != retval)
-    {
-        goto cleanup_tev;
-    }
-
-    /* clear the file / position cache. */
-    file_position_cache_clear(scanner->cache);
-
-    /* we are now in the init state. */
-    scanner->state = CPARSE_PREPROCESSOR_SCANNER_STATE_INIT;
-
-    /* success. */
-    goto cleanup_tev;
-
-cleanup_tev:
-    release_retval = event_dispose(&tev);
-    if (STATUS_SUCCESS != release_retval)
-    {
-        retval = release_retval;
-    }
-
-done:
-    if (STATUS_SUCCESS != retval)
-    {
-        return retval;
-    }
-
-    /* if we succeed, then recursively process the new event on the way out. */
-    return preprocessor_scanner_event_callback(scanner, ev);
-}
-
-/**
- * \brief Broadcast a greater than token.
- *
- * \param scanner           The scanner for this operation.
- * \param ev                The event to process AFTER this token.
- *
- * \returns a status code indicating success or failure.
- *      - STATUS_SUCCESS on success.
- *      - a non-zero error code on failure.
- */
-static int broadcast_greater_than_token(
-    preprocessor_scanner* scanner, const event* ev)
-{
-    int retval, release_retval;
-    const cursor* pos;
-    event tev;
-
-    /* get the cached position. */
-    retval = file_position_cache_position_get(scanner->cache, &pos);
-    if (STATUS_SUCCESS != retval)
-    {
-        goto done;
-    }
-
-    /* initialize the token event. */
-    retval = event_init_for_token_greater_than(&tev, pos);
     if (STATUS_SUCCESS != retval)
     {
         goto done;
