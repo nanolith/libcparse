fdbcf0a860d502e9f8f2138a66828b5e401b03c4
Justin Handville <nanolith@gmail.com>
2024-02-10 22:48:51 -0500

    signed int integer type decl.

---
+++ include/libcparse/integer_type.h
@@ -0,0 +1,30 @@
+/**
+ * \file libcparse/integer_type.h
+ *
+ * \brief The integer_type enumeration describes the type of an integer.
+ *
+ * \copyright 2023 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <libcparse/function_decl.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+/**
+ * \brief The integer type describes the type of an integer constant.
+ */
+enum CPARSE_SYM(integer_type)
+{
+    CPARSE_INTEGER_TYPE_SIGNED_INT =                                    0x0000,
+};
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
