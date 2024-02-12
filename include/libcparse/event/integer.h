ada3425661c10d7c32c2839473d3155acfb47480
Justin Handville <nanolith@gmail.com>
2024-02-11 23:07:53 -0500

    event_integer_token_init_for_unsigned_long_long decl.

--- include/libcparse/event/integer.h
+++ include/libcparse/event/integer.h
@@ -108,6 +108,22 @@ int CPARSE_SYM(event_integer_token_init_for_signed_long_long)(
     CPARSE_SYM(event_integer_token)* ev, const CPARSE_SYM(cursor)* cursor,
     long long val);
 
+/**
+ * \brief Perform an in-place initialization of an unsigned long long \ref
+ * event_integer_token instance.
+ *
+ * \param ev                Pointer to the event to initialize.
+ * \param cursor            The event cursor.
+ * \param val               The value of this token
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero failure code on failure.
+ */
+int CPARSE_SYM(event_integer_token_init_for_unsigned_long_long)(
+    CPARSE_SYM(event_integer_token)* ev, const CPARSE_SYM(cursor)* cursor,
+    unsigned long long val);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
