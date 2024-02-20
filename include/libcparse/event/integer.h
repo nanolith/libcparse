cf0aedd6099f1232ed395abb289afc4d8d3a9e38
Justin Handville <nanolith@gmail.com>
2024-02-19 21:19:16 -0500

    event_integer_token_coerce_unsigned_long export.

--- include/libcparse/event/integer.h
+++ include/libcparse/event/integer.h
@@ -785,6 +785,10 @@ CPARSE_SYM(event)* CPARSE_SYM(event_integer_token_upcast)(
     static inline long sym ## event_integer_token_coerce_long( \
         const CPARSE_SYM(event_integer_token)* x) { \
             return CPARSE_SYM(event_integer_token_coerce_long)(x); } \
+    static inline unsigned long \
+    sym ## event_integer_token_coerce_unsigned_long( \
+        const CPARSE_SYM(event_integer_token)* x) { \
+            return CPARSE_SYM(event_integer_token_coerce_unsigned_long)(x); } \
     static inline int sym ## event_downcast_to_event_integer_token( \
         CPARSE_SYM(event_integer_token)** x, CPARSE_SYM(event)* y) { \
             return CPARSE_SYM(event_downcast_to_event_integer_token)(x,y); } \
