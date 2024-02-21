78e792b470ddd831ed667e3737e9846327d5c3c0
Justin Handville <nanolith@gmail.com>
2024-02-20 23:32:00 -0500

    event_integer_token_coerce_uint64 export.

--- include/libcparse/event/integer.h
+++ include/libcparse/event/integer.h
@@ -819,6 +819,9 @@ CPARSE_SYM(event)* CPARSE_SYM(event_integer_token_upcast)(
     static inline int64_t sym ## event_integer_token_coerce_int64( \
         const CPARSE_SYM(event_integer_token)* x) { \
             return CPARSE_SYM(event_integer_token_coerce_int64)(x); } \
+    static inline uint64_t sym ## event_integer_token_coerce_uint64( \
+        const CPARSE_SYM(event_integer_token)* x) { \
+            return CPARSE_SYM(event_integer_token_coerce_uint64)(x); } \
     static inline int sym ## event_downcast_to_event_integer_token( \
         CPARSE_SYM(event_integer_token)** x, CPARSE_SYM(event)* y) { \
             return CPARSE_SYM(event_downcast_to_event_integer_token)(x,y); } \
