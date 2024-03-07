78da16c43aa35d73f88cede67ca4dbb43a06912e
Justin Handville <nanolith@gmail.com>
2024-03-06 21:29:47 -0500

    event_init_for_token_keyword__Bool export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -1274,6 +1274,9 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
     static inline int sym ## event_init_for_token_keyword__Atomic( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_token_keyword__Atomic)(x,y); } \
+    static inline int sym ## event_init_for_token_keyword__Bool( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return CPARSE_SYM(event_init_for_token_keyword__Bool)(x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
