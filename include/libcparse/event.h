f773b34dd370feab5109335ab15a02ac366e058c
Justin Handville <nanolith@gmail.com>
2024-03-06 21:40:12 -0500

    event_init_for_token_keyword__Static_assert decl.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -1064,6 +1064,20 @@ int CPARSE_SYM(event_init_for_token_keyword__Noreturn)(
     CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
 
 /**
+ * \brief Perform an in-place initialization of a _Static_assert keyword token
+ * event instance.
+ *
+ * \param ev                    Pointer to the event to initialize.
+ * \param cursor                The event cursor.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(event_init_for_token_keyword__Static_assert)(
+    CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
+
+/**
  * \brief Perform an in-place disposal of an event instance.
  *
  * \param ev                    Pointer to the event to dispose.
