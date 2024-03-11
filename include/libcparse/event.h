d18c14a49f2b38f9375195e661df8cef0ef0f75e
Justin Handville <nanolith@gmail.com>
2024-03-10 23:41:37 -0400

    event_init_for_token_keyword_if decl.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -1302,6 +1302,20 @@ int CPARSE_SYM(event_init_for_token_keyword_goto)(
     CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
 
 /**
+ * \brief Perform an in-place initialization of an if keyword token event
+ * instance.
+ *
+ * \param ev                    Pointer to the event to initialize.
+ * \param cursor                The event cursor.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(event_init_for_token_keyword_if)(
+    CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
+
+/**
  * \brief Perform an in-place disposal of an event instance.
  *
  * \param ev                    Pointer to the event to dispose.
