86bd85ccc3d11ceca23649b777992f05b43d8fc8
Justin Handville <nanolith@gmail.com>
2024-03-11 22:57:50 -0400

    event_init_for_token_keyword_register export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -1719,6 +1719,9 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
     static inline int sym ## event_init_for_token_keyword_long( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_token_keyword_long)(x,y); } \
+    static inline int sym ## event_init_for_token_keyword_register( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return CPARSE_SYM(event_init_for_token_keyword_register)(x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
