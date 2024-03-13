462d2f3614a9b6bc4c132f786870a06f6acabea2
Justin Handville <nanolith@gmail.com>
2024-03-12 22:53:22 -0400

    event_init_for_token_keyword_struct decl.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -1456,6 +1456,20 @@ int CPARSE_SYM(event_init_for_token_keyword_static)(
     CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
 
 /**
+ * \brief Perform an in-place initialization of a struct keyword token event
+ * instance.
+ *
+ * \param ev                    Pointer to the event to initialize.
+ * \param cursor                The event cursor.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(event_init_for_token_keyword_struct)(
+    CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
+
+/**
  * \brief Perform an in-place disposal of an event instance.
  *
  * \param ev                    Pointer to the event to dispose.
