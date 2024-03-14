3bd853de645e859aa24396f76d06e77905967b7e
Justin Handville <nanolith@gmail.com>
2024-03-13 22:07:01 -0400

    event_init_for_token_keyword_unsigned decl.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -1512,6 +1512,20 @@ int CPARSE_SYM(event_init_for_token_keyword_union)(
     CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
 
 /**
+ * \brief Perform an in-place initialization of a unsigned keyword token event
+ * instance.
+ *
+ * \param ev                    Pointer to the event to initialize.
+ * \param cursor                The event cursor.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(event_init_for_token_keyword_unsigned)(
+    CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
+
+/**
  * \brief Perform an in-place disposal of an event instance.
  *
  * \param ev                    Pointer to the event to dispose.
