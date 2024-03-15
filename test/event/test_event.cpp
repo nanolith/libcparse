7f9b31374a69b98a02bc2a2183051a9958a7525d
Justin Handville <nanolith@gmail.com>
2024-03-14 21:44:18 -0400

    _Atomic keyword token event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -1289,3 +1289,26 @@ TEST(_Alignof_keyword_token)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create an _Atomic keyword token event.
+ */
+TEST(_Atomic_keyword_token)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_keyword__Atomic(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_KEYWORD__ATOMIC == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
