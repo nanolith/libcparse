d8a3ce18065d1e47806da2a34adcb6a1c3c9418d
Justin Handville <nanolith@gmail.com>
2024-03-15 22:35:54 -0400

    _Complex keyword token event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -1335,3 +1335,26 @@ TEST(_Bool_keyword_token)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create an _Complex keyword token event.
+ */
+TEST(_Complex_keyword_token)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_keyword__Complex(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_KEYWORD__COMPLEX == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
