cbc5fc0d481273e9a69d781784f7ae2626747b2b
Justin Handville <nanolith@gmail.com>
2024-03-18 23:08:42 -0400

    for keyword token event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -1772,3 +1772,26 @@ TEST(float_keyword_token)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create a for keyword token event.
+ */
+TEST(for_keyword_token)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_keyword_for(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_KEYWORD_FOR == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
