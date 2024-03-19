811589cf4c2baaebccf9dced88bbd1c06fc5669d
Justin Handville <nanolith@gmail.com>
2024-03-18 23:04:23 -0400

    extern keyword token event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -1726,3 +1726,26 @@ TEST(enum_keyword_token)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create an extern keyword token event.
+ */
+TEST(extern_keyword_token)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_keyword_extern(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_KEYWORD_EXTERN == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
