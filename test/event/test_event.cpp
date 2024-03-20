0d6945b347c2a55c95965cc8988a7440f50c21a5
Justin Handville <nanolith@gmail.com>
2024-03-20 19:38:40 -0400

    register keyword token event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -1910,3 +1910,26 @@ TEST(long_keyword_token)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create a register keyword token event.
+ */
+TEST(register_keyword_token)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_keyword_register(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_KEYWORD_REGISTER == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
