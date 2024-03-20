c0dbec2b63805754d0017d435eefcc64a975f5f8
Justin Handville <nanolith@gmail.com>
2024-03-19 20:28:32 -0400

    int keyword token event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -1864,3 +1864,26 @@ TEST(inline_keyword_token)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create an int keyword token event.
+ */
+TEST(int_keyword_token)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_keyword_int(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_KEYWORD_INT == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
