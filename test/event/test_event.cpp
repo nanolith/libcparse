3be1f006e2949682b30238ab3e801f24e6783c1d
Justin Handville <nanolith@gmail.com>
2024-03-21 21:00:32 -0400

    signed keyword token event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -2002,3 +2002,26 @@ TEST(short_keyword_token)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create a signed keyword token event.
+ */
+TEST(signed_keyword_token)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_keyword_signed(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_KEYWORD_SIGNED == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
