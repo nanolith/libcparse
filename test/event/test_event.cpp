c0d704271da83a4c09bb96f5f7231bd3391dda97
Justin Handville <nanolith@gmail.com>
2024-03-22 22:35:56 -0400

    union keyword token event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -2140,3 +2140,26 @@ TEST(typedef_keyword_token)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create a union keyword token event.
+ */
+TEST(union_keyword_token)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_keyword_union(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_KEYWORD_UNION == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
