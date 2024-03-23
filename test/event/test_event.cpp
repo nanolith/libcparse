fdb92271b4ba7e4934d5d4d44cde999b0f33739b
Justin Handville <nanolith@gmail.com>
2024-03-22 22:37:41 -0400

    unsigned keyword token event unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -2163,3 +2163,26 @@ TEST(union_keyword_token)
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
 }
+
+/**
+ * Test that we can create a unsigned keyword token event.
+ */
+TEST(unsigned_keyword_token)
+{
+    event ev;
+    cursor c;
+
+    /* clear the cursor. */
+    memset(&c, 0, sizeof(c));
+
+    /* Initialize an event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_init_for_token_keyword_unsigned(&ev, &c));
+
+    /* The event type is correct. */
+    TEST_EXPECT(
+        CPARSE_EVENT_TYPE_TOKEN_KEYWORD_UNSIGNED == event_get_type(&ev));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
