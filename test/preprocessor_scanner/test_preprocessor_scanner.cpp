dd5b6a5cd69cfeef19cf18c4c73acedf58d28e7f
Justin Handville <nanolith@gmail.com>
2024-03-31 19:41:04 -0400

    Added void keyword scan unit test.

--- test/preprocessor_scanner/test_preprocessor_scanner.cpp
+++ test/preprocessor_scanner/test_preprocessor_scanner.cpp
@@ -334,6 +334,9 @@ static int dummy_callback(void* context, const CPARSE_SYM(event)* ev)
         case CPARSE_EVENT_TYPE_TOKEN_KEYWORD_UNSIGNED:
             ctx->vals.push_back(make_pair(token_type, "unsigned"));
             break;
+        case CPARSE_EVENT_TYPE_TOKEN_KEYWORD_VOID:
+            ctx->vals.push_back(make_pair(token_type, "void"));
+            break;
 
         default:
             return -1;
@@ -6388,6 +6391,69 @@ TEST(unsigned_keyword_token)
 
     /* this first value is an unsigned keyword. */
     TEST_EXPECT(CPARSE_EVENT_TYPE_TOKEN_KEYWORD_UNSIGNED == f->first);
+
+    /* clean up. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == preprocessor_scanner_release(scanner));
+    TEST_ASSERT(STATUS_SUCCESS == event_handler_dispose(&eh));
+}
+
+/**
+ * Test that we can scan a void keyword.
+ */
+TEST(void_keyword_token)
+{
+    preprocessor_scanner* scanner;
+    input_stream* stream;
+    event_handler eh;
+    test_context t1;
+    const char* INPUT_STRING = "void";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == preprocessor_scanner_create(&scanner));
+
+    /* create an event handler. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_handler_init(&eh, &dummy_callback, &t1));
+
+    /* get the abstract parser. */
+    auto ap = preprocessor_scanner_upcast(scanner);
+
+    /* subscribe to the scanner. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == abstract_parser_preprocessor_scanner_subscribe(ap, &eh));
+
+    /* create an input stream. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == input_stream_create_from_string(&stream, INPUT_STRING));
+
+    /* add the input stream to the parser. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == abstract_parser_push_input_stream(ap, "stdin", stream));
+
+    /* precondition: eof is false. */
+    TEST_ASSERT(!t1.eof);
+
+    /* precondition: vals is empty. */
+    TEST_ASSERT(t1.vals.empty());
+
+    /* run the filter. */
+    TEST_ASSERT(STATUS_SUCCESS == abstract_parser_run(ap));
+
+    /* postcondition: eof is true. */
+    TEST_EXPECT(t1.eof);
+
+    /* postcondition: there is one value in vals. */
+    TEST_ASSERT(1 == t1.vals.size());
+    auto f = t1.vals.begin();
+    TEST_ASSERT(f != t1.vals.end());
+
+    /* this first value is a void keyword. */
+    TEST_EXPECT(CPARSE_EVENT_TYPE_TOKEN_KEYWORD_VOID == f->first);
 
     /* clean up. */
     TEST_ASSERT(
