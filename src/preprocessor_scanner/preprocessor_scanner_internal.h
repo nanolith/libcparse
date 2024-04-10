2e23d8829573fe97abf4e3faba6d0f34ead620c5
Justin Handville <nanolith@gmail.com>
2024-04-09 22:06:19 -0400

    Added hex long integer support to scanner.

--- src/preprocessor_scanner/preprocessor_scanner_internal.h
+++ src/preprocessor_scanner/preprocessor_scanner_internal.h
@@ -64,6 +64,7 @@ enum CPARSE_SYM(preprocessor_scanner_state)
     CPARSE_PREPROCESSOR_SCANNER_STATE_IN_DECIMAL_INTEGER_END =         25,
     CPARSE_PREPROCESSOR_SCANNER_STATE_IN_HEX_INTEGER_U =               26,
     CPARSE_PREPROCESSOR_SCANNER_STATE_IN_HEX_INTEGER_UL =              27,
+    CPARSE_PREPROCESSOR_SCANNER_STATE_IN_HEX_INTEGER_L =               28,
 };
 
 /******************************************************************************/
