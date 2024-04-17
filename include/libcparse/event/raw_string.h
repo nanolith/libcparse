1b9255d3022a9589aa6d3835fe7d4ae1357768b1
Justin Handville <nanolith@gmail.com>
2024-04-17 19:59:22 -0400

    event_downcast_to_event_raw_string_token decl.

--- include/libcparse/event/raw_string.h
+++ include/libcparse/event/raw_string.h
@@ -93,6 +93,21 @@ int CPARSE_SYM(event_raw_string_token_convert)(
     CPARSE_SYM(event_string)* s_ev,
     const CPARSE_SYM(event_raw_string_token)* ev);
 
+/**
+ * \brief Attempt to downcast an \ref event to an \ref event_raw_string_token.
+ *
+ * \param s_ev              Pointer to the event pointer to receive the downcast
+ *                          instance on success.
+ * \param ev                The \ref event pointer to attempt to downcast to the
+ *                          derived type.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(event_downcast_to_event_raw_string_token)(
+    CPARSE_SYM(event_raw_string_token)** s_ev, CPARSE_SYM(event)* ev);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
