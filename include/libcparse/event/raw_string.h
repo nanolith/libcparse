65a3fb7aee72574d4fdf8f9a097b6d24bd0720d8
Justin Handville <nanolith@gmail.com>
2024-04-17 20:04:24 -0400

    event_raw_string_token_init_for_system_string decl.

--- include/libcparse/event/raw_string.h
+++ include/libcparse/event/raw_string.h
@@ -143,6 +143,12 @@ CPARSE_SYM(event)* CPARSE_SYM(event_raw_string_token_upcast)(
         CPARSE_SYM(event_raw_string_token)* x, const CPARSE_SYM(cursor)* y, \
         const char* z) { \
             return CPARSE_SYM(event_raw_string_token_init)(x,y,z); } \
+    static inline int sym ## event_raw_string_token_init_for_system_string( \
+        CPARSE_SYM(event_raw_string_token)* x, const CPARSE_SYM(cursor)* y, \
+        const char* z) { \
+            return \
+                CPARSE_SYM(event_raw_string_token_init_for_system_string)( \
+                    x,y,z); } \
     CPARSE_END_EXPORT \
     REQUIRE_SEMICOLON_HERE
 #define CPARSE_IMPORT_event_raw_string_as(sym) \
