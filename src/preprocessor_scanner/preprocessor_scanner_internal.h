07b8ad0e2ba8baf1ef3926213b2be4c021020089
Justin Handville <nanolith@gmail.com>
2024-04-26 22:19:01 -0400

    string big u7 state.

--- src/preprocessor_scanner/preprocessor_scanner_internal.h
+++ src/preprocessor_scanner/preprocessor_scanner_internal.h
@@ -79,6 +79,7 @@ enum CPARSE_SYM(preprocessor_scanner_state)
     CPARSE_PREPROCESSOR_SCANNER_STATE_IN_STRING_BIG_U4 =               40,
     CPARSE_PREPROCESSOR_SCANNER_STATE_IN_STRING_BIG_U5 =               41,
     CPARSE_PREPROCESSOR_SCANNER_STATE_IN_STRING_BIG_U6 =               42,
+    CPARSE_PREPROCESSOR_SCANNER_STATE_IN_STRING_BIG_U7 =               43,
 };
 
 /******************************************************************************/
