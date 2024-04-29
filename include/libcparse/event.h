9209dafd354578a5ad9a7a3f695d43a0de6e8c1d
Justin Handville <nanolith@gmail.com>
2024-04-29 19:56:48 -0400

    event_init_for_preprocessor_directive_end export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -1974,6 +1974,10 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
     static inline int sym ## event_init_for_token_keyword_while( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_token_keyword_while)(x,y); } \
+    static inline int sym ## event_init_for_preprocessor_directive_end( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return \
+                CPARSE_SYM(event_init_for_preprocessor_directive_end)(x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
