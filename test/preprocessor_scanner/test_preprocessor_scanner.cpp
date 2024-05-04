6d8708e98fc5c5f53b95c59500af684f1e30fcfd
Justin Handville <nanolith@gmail.com>
2024-05-03 21:32:51 -0400

    Added #else token unit test.

--- test/preprocessor_scanner/test_preprocessor_scanner.cpp
+++ test/preprocessor_scanner/test_preprocessor_scanner.cpp
@@ -392,6 +392,9 @@ static int dummy_callback(void* context, const CPARSE_SYM(event)* ev)
         case CPARSE_EVENT_TYPE_TOKEN_PP_ID_ELIF:
             ctx->vals.push_back(make_pair(token_type, "#elif"));
             break;
+        case CPARSE_EVENT_TYPE_TOKEN_PP_ID_ELSE:
+            ctx->vals.push_back(make_pair(token_type, "#else"));
+            break;
         case CPARSE_EVENT_TYPE_TOKEN_PP_ID_ENDIF:
             ctx->vals.push_back(make_pair(token_type, "#endif"));
             break;
@@ -16865,6 +16868,75 @@ TEST(pp_elif_FOO_tokens)
     ++f;
 
     /* the third value is an end preprocessor event. */
+    TEST_EXPECT(CPARSE_EVENT_TYPE_PP_END == f->first);
+
+    /* clean up. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == preprocessor_scanner_release(scanner));
+    TEST_ASSERT(STATUS_SUCCESS == event_handler_dispose(&eh));
+}
+
+/**
+ * Test that we can scan an #else token.
+ */
+TEST(pp_else_FOO_tokens)
+{
+    preprocessor_scanner* scanner;
+    input_stream* stream;
+    event_handler eh;
+    test_context t1;
+    const char* INPUT_STRING = "#else";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == preprocessor_scanner_create(&scanner));
+
+    /* create an event handler. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_handler_init(&eh, &dummy_callback, &t1));
+
+    /* get the abstract parser. */
+    auto ap = preprocessor_scanner_upcast(scanner);
+
+    /* subscribe to the scanner. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == abstract_parser_preprocessor_scanner_subscribe(ap, &eh));
+
+    /* create an input stream. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == input_stream_create_from_string(&stream, INPUT_STRING));
+
+    /* add the input stream to the parser. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == abstract_parser_push_input_stream(ap, "stdin", stream));
+
+    /* precondition: eof is false. */
+    TEST_ASSERT(!t1.eof);
+
+    /* precondition: vals is empty. */
+    TEST_ASSERT(t1.vals.empty());
+
+    /* run the filter. */
+    TEST_ASSERT(STATUS_SUCCESS == abstract_parser_run(ap));
+
+    /* postcondition: eof is true. */
+    TEST_EXPECT(t1.eof);
+
+    /* postcondition: there are two values in vals. */
+    TEST_ASSERT(2 == t1.vals.size());
+    auto f = t1.vals.begin();
+    TEST_ASSERT(f != t1.vals.end());
+
+    /* the first value is an else token. */
+    TEST_EXPECT(CPARSE_EVENT_TYPE_TOKEN_PP_ID_ELSE == f->first);
+    TEST_EXPECT("#else" == f->second);
+
+    ++f;
+
+    /* the second value is an end preprocessor event. */
     TEST_EXPECT(CPARSE_EVENT_TYPE_PP_END == f->first);
 
     /* clean up. */
