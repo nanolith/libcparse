ae2384511fd86e3f96a7033a17b40c9983e55d4f
Justin Handville <nanolith@gmail.com>
2024-05-04 22:30:18 -0400

    Added #line 1234 unit test.

--- test/preprocessor_scanner/test_preprocessor_scanner.cpp
+++ test/preprocessor_scanner/test_preprocessor_scanner.cpp
@@ -398,6 +398,9 @@ static int dummy_callback(void* context, const CPARSE_SYM(event)* ev)
         case CPARSE_EVENT_TYPE_TOKEN_PP_ID_ELSE:
             ctx->vals.push_back(make_pair(token_type, "#else"));
             break;
+        case CPARSE_EVENT_TYPE_TOKEN_PP_ID_LINE:
+            ctx->vals.push_back(make_pair(token_type, "#line"));
+            break;
         case CPARSE_EVENT_TYPE_TOKEN_PP_ID_ENDIF:
             ctx->vals.push_back(make_pair(token_type, "#endif"));
             break;
@@ -17089,6 +17092,81 @@ TEST(pp_undef_FOO_tokens)
     /* the second value is an identifier. */
     TEST_EXPECT(CPARSE_EVENT_TYPE_TOKEN_IDENTIFIER == f->first);
     TEST_EXPECT("FOO" == f->second);
+
+    ++f;
+
+    /* the third value is an end preprocessor event. */
+    TEST_EXPECT(CPARSE_EVENT_TYPE_PP_END == f->first);
+
+    /* clean up. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == preprocessor_scanner_release(scanner));
+    TEST_ASSERT(STATUS_SUCCESS == event_handler_dispose(&eh));
+}
+
+/**
+ * Test that we can scan #line 1234 expression tokens.
+ */
+TEST(pp_line_1234_tokens)
+{
+    preprocessor_scanner* scanner;
+    input_stream* stream;
+    event_handler eh;
+    test_context t1;
+    const char* INPUT_STRING = "#line 1234";
+
+    /* Create the scanner instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == preprocessor_scanner_create(&scanner));
+
+    /* create an event handler. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_handler_init(&eh, &dummy_callback, &t1));
+
+    /* get the abstract parser. */
+    auto ap = preprocessor_scanner_upcast(scanner);
+
+    /* subscribe to the scanner. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == abstract_parser_preprocessor_scanner_subscribe(ap, &eh));
+
+    /* create an input stream. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == input_stream_create_from_string(&stream, INPUT_STRING));
+
+    /* add the input stream to the parser. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == abstract_parser_push_input_stream(ap, "stdin", stream));
+
+    /* precondition: eof is false. */
+    TEST_ASSERT(!t1.eof);
+
+    /* precondition: vals is empty. */
+    TEST_ASSERT(t1.vals.empty());
+
+    /* run the filter. */
+    TEST_ASSERT(STATUS_SUCCESS == abstract_parser_run(ap));
+
+    /* postcondition: eof is true. */
+    TEST_EXPECT(t1.eof);
+
+    /* postcondition: there are two values in vals. */
+    TEST_ASSERT(3 == t1.vals.size());
+    auto f = t1.vals.begin();
+    TEST_ASSERT(f != t1.vals.end());
+
+    /* the first value is a line token. */
+    TEST_EXPECT(CPARSE_EVENT_TYPE_TOKEN_PP_ID_LINE == f->first);
+    TEST_EXPECT("#line" == f->second);
+
+    ++f;
+
+    /* the second value is a raw integer. */
+    TEST_EXPECT(CPARSE_EVENT_TYPE_TOKEN_VALUE_RAW_INTEGER == f->first);
+    TEST_EXPECT("1234" == f->second);
 
     ++f;
 
