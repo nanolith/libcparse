97970e15b25da9e4810963e63046ab2a9557686c
Justin Handville <nanolith@gmail.com>
2024-05-04 22:28:35 -0400

    event_init_for_token_preprocessor_id_line impl.

---
+++ src/event/event_init_for_token_preprocessor_id_line.c
@@ -0,0 +1,32 @@
+/**
+ * \file src/event/event_init_for_token_preprocessor_id_line.c
+ *
+ * \brief Initialize an event instance for the preprocessor id line token
+ * event.
+ *
+ * \copyright 2023 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <libcparse/event_type.h>
+
+#include "event_internal.h"
+
+CPARSE_IMPORT_event_internal;
+
+/**
+ * \brief Perform an in-place initialization of a preprocessor id line token
+ * event instance.
+ *
+ * \param ev                    Pointer to the event to initialize.
+ * \param cursor                The event cursor.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(event_init_for_token_preprocessor_id_line)(
+    CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor)
+{
+    return event_init(ev, CPARSE_EVENT_TYPE_TOKEN_PP_ID_LINE, cursor);
+}
