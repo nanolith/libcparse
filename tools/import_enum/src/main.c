bbf4ee543a03688903491963f0f97749bfbcb958
Justin Handville <nanolith@gmail.com>
2024-05-06 21:50:13 -0400

    stub for import_enum tool.

---
+++ tools/import_enum/src/main.c
@@ -0,0 +1,32 @@
+/**
+ * \file tools/import_enum/src/main.c
+ *
+ * \brief Main entry point for the import_enum tool.
+ *
+ * import_enum is a temporary tool for importing enumerations into the proof
+ * assistant so we can perform proofs based on these enumerations.
+ *
+ * \copyright 2023 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <stdio.h>
+
+/**
+ * \brief Main entry point for import_enum.
+ *
+ * \param argc              The argument count.
+ * \param argv              The argument vector.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    /* TODO - implement. */
+    fprintf(stderr, "error: not yet implemented.\n");
+
+    return 1;
+}
