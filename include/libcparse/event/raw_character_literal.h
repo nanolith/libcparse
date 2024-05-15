cbbdbb14b65cbbd6972976b78c5fb160b3647cb2
Justin Handville <nanolith@gmail.com>
2024-05-14 23:34:29 -0400

    event_downcast_to_event_raw_character_literal decl.

--- include/libcparse/event/raw_character_literal.h
+++ include/libcparse/event/raw_character_literal.h
@@ -11,6 +11,7 @@
 #pragma once
 
 #include <libcparse/cursor.h>
+#include <libcparse/event_fwd.h>
 #include <libcparse/function_decl.h>
 
 /* C++ compatibility. */
@@ -72,6 +73,23 @@ int CPARSE_SYM(event_raw_character_literal_dispose)(
  */
 const char* CPARSE_SYM(event_raw_character_literal_get)(
     const CPARSE_SYM(event_raw_character_literal)* ev);
+
+
+/**
+ * \brief Attempt to downcast an \ref event to an
+ * \ref event_raw_character_literal.
+ *
+ * \param c_ev              Pointer to the event pointer to receive the downcast
+ *                          instance on success.
+ * \param ev                The \ref event pointer to attempt to downcast to the
+ *                          derived type.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(event_downcast_to_event_raw_character_literal)(
+    CPARSE_SYM(event_raw_character_literal)** c_ev, CPARSE_SYM(event)* ev);
 
 /* C++ compatibility. */
 # ifdef   __cplusplus
