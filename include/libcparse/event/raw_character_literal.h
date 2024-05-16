19ef0f3c73cb4b8c92b614339a5b3e7e215aa6bb
Justin Handville <nanolith@gmail.com>
2024-05-15 22:29:24 -0400

    Make export inline.

--- include/libcparse/event/raw_character_literal.h
+++ include/libcparse/event/raw_character_literal.h
@@ -115,7 +115,7 @@ CPARSE_SYM(event)* CPARSE_SYM(event_raw_character_literal_upcast)(
     static inline int sym ## event_raw_character_literal_dispose( \
         CPARSE_SYM(event_raw_character_literal)* x) { \
             return CPARSE_SYM(event_raw_character_literal_dispose)(x); } \
-    static const char* sym ## event_raw_character_literal_get( \
+    static inline const char* sym ## event_raw_character_literal_get( \
         const CPARSE_SYM(event_raw_character_literal)* x) { \
             return CPARSE_SYM(event_raw_character_literal_get)(x); } \
     CPARSE_END_EXPORT \
