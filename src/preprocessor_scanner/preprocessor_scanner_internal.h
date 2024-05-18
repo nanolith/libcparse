02a0b38bdfa721888882ae1be1d1e8549f633b9a
Justin Handville <nanolith@gmail.com>
2024-05-17 22:59:28 -0400

    Added char slash state.

--- src/preprocessor_scanner/preprocessor_scanner_internal.h
+++ src/preprocessor_scanner/preprocessor_scanner_internal.h
@@ -88,6 +88,7 @@ enum CPARSE_SYM(preprocessor_scanner_state)
     CPARSE_PREPROCESSOR_SCANNER_STATE_IN_HASH =                        45,
     CPARSE_PREPROCESSOR_SCANNER_STATE_IN_SINGLE_HASH =                 46,
     CPARSE_PREPROCESSOR_SCANNER_STATE_IN_CHAR =                        47,
+    CPARSE_PREPROCESSOR_SCANNER_STATE_IN_CHAR_SLASH =                  48,
 };
 
 enum CPARSE_SYM(preprocessor_directive_state)
