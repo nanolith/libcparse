78397e6a9913d274daf1e66f4eb715c466218597
Justin Handville <nanolith@gmail.com>
2024-05-21 21:27:52 -0400

    Added big U char state.

--- src/preprocessor_scanner/preprocessor_scanner_internal.h
+++ src/preprocessor_scanner/preprocessor_scanner_internal.h
@@ -97,6 +97,7 @@ enum CPARSE_SYM(preprocessor_scanner_state)
     CPARSE_PREPROCESSOR_SCANNER_STATE_IN_CHAR_LITTLE_U1 =              54,
     CPARSE_PREPROCESSOR_SCANNER_STATE_IN_CHAR_LITTLE_U2 =              55,
     CPARSE_PREPROCESSOR_SCANNER_STATE_IN_CHAR_LITTLE_U3 =              56,
+    CPARSE_PREPROCESSOR_SCANNER_STATE_IN_CHAR_BIG_U =                  57,
 };
 
 enum CPARSE_SYM(preprocessor_directive_state)
