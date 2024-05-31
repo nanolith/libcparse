155a82272bd0c6beeda76a9f332cb28cf0122658
Justin Handville <nanolith@gmail.com>
2024-05-30 22:28:57 -0400

    Added command-line parsing to main.

--- examples/syntax_highlight/src/main.c
+++ examples/syntax_highlight/src/main.c
@@ -10,9 +10,12 @@
  * distribution for the license terms under which this software is distributed.
  */
 
+#include <libcparse/status_codes.h>
 #include <stdio.h>
 
 #include "syntax_highlight_internal.h"
+
+static void log_config(const syntax_highlight_config* config);
 
 /**
  * \brief Main entry point for comment_strip.
@@ -24,10 +27,47 @@
  */
 int main(int argc, char* argv[])
 {
-    (void)argc;
-    (void)argv;
+    int retval, release_retval;
+    syntax_highlight_config* config = NULL;
+
+    /* read command-line options. */
+    retval = syntax_highlight_config_create(&config, &argc, &argv);
+    if (STATUS_SUCCESS != retval)
+    {
+        retval = 1;
+        goto done;
+    }
+
+    log_config(config);
 
     fprintf(stderr, "Not yet implemented.\n");
+    retval = 1;
+    goto cleanup;
 
-    return 1;
+cleanup:
+    release_retval = syntax_highlight_config_release(config);
+    if (STATUS_SUCCESS != release_retval)
+    {
+        retval = release_retval;
+    }
+
+done:
+    return retval;
+}
+
+/**
+ * \brief Log config values to stderr.
+ *
+ * \params config       The config to log.
+ */
+static void log_config(const syntax_highlight_config* config)
+{
+    fprintf(stderr, "Input file: %s\n", config->input);
+    fprintf(stderr, "Output file: %s\n", config->output);
+
+    if (0 != config->snip_begin)
+    {
+        fprintf(stderr, "Snip begin: %ld\n", config->snip_begin);
+        fprintf(stderr, "Snip end: %ld\n", config->snip_end);
+    }
 }
