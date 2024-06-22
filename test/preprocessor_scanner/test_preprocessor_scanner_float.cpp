d7f3aa91d305cfd9cd65d8210ac9c5f8197e4156
Justin Handville <nanolith@gmail.com>
2024-06-21 21:58:13 -0400

    0X0.P0 unit test.

--- test/preprocessor_scanner/test_preprocessor_scanner_float.cpp
+++ test/preprocessor_scanner/test_preprocessor_scanner_float.cpp
@@ -135,6 +135,7 @@ FLOAT_TEST_EXPECT_SUCCESS(HEX_float_one_dot_one_p0,     "0X1.1p0");
 FLOAT_TEST_EXPECT_SUCCESS(HEX_float_1_dot_1_p_neg_5,    "0X1.1p-5");
 FLOAT_TEST_EXPECT_SUCCESS(HEX_float_1_dot_1_p_pos_5,    "0X1.1p+5");
 FLOAT_TEST_EXPECT_SUCCESS(HEX_float_dot_p0,             "0X.p0");
+FLOAT_TEST_EXPECT_SUCCESS(HEX_float_zero_dot_P0,        "0X0.P0");
 
 #define expect_digit ERROR_LIBCPARSE_PP_SCANNER_EXPECTING_DIGIT
 #define expect_p     ERROR_LIBCPARSE_PP_SCANNER_HEX_FLOAT_EXPECTING_P
