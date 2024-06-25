63fad889434153b36992fed094341e401b485357
Justin Handville <nanolith@gmail.com>
2024-06-24 21:24:23 -0400

    0X1.P- unit test.

--- test/preprocessor_scanner/test_preprocessor_scanner_float.cpp
+++ test/preprocessor_scanner/test_preprocessor_scanner_float.cpp
@@ -178,3 +178,4 @@ FLOAT_TEST_EXPECT_FAILURE(HEX_no_pos_p_digit,   "0X1.p+",   expect_digit);
 FLOAT_TEST_EXPECT_FAILURE(HEX_no_neg_p_digit,   "0X1.p-",   expect_digit);
 FLOAT_TEST_EXPECT_FAILURE(HEX_no_P_digit,       "0X1.P",    expect_digit);
 FLOAT_TEST_EXPECT_FAILURE(HEX_no_pos_P_digit,   "0X1.P+",   expect_digit);
+FLOAT_TEST_EXPECT_FAILURE(HEX_no_neg_P_digit,   "0X1.P-",   expect_digit);
