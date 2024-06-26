0190b23d01cc047c3084aee8f8e1d7a344f4e0a5
Justin Handville <nanolith@gmail.com>
2024-06-25 21:46:23 -0400

    float l suffix unit test.

--- test/preprocessor_scanner/test_preprocessor_scanner_float.cpp
+++ test/preprocessor_scanner/test_preprocessor_scanner_float.cpp
@@ -153,6 +153,7 @@ FLOAT_TEST_EXPECT_SUCCESS(HEX_float_1_1_P_neg_5,        "0X11P-5");
 FLOAT_TEST_EXPECT_SUCCESS(HEX_float_1_1_P_pos_5,        "0X11P+5");
 FLOAT_TEST_EXPECT_SUCCESS(float_zero_dot_zero_suffix_f, "0.0f");
 FLOAT_TEST_EXPECT_SUCCESS(float_zero_dot_zero_suffix_F, "0.0F");
+FLOAT_TEST_EXPECT_SUCCESS(float_zero_dot_zero_suffix_l, "0.0l");
 
 #define expect_digit ERROR_LIBCPARSE_PP_SCANNER_EXPECTING_DIGIT
 #define expect_p     ERROR_LIBCPARSE_PP_SCANNER_HEX_FLOAT_EXPECTING_P
