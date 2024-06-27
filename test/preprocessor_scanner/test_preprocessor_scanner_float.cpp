76cdeb35d800af98c470ab9e1c1e8de489252bd6
Justin Handville <nanolith@gmail.com>
2024-06-26 20:11:01 -0400

    .0L unit test.

--- test/preprocessor_scanner/test_preprocessor_scanner_float.cpp
+++ test/preprocessor_scanner/test_preprocessor_scanner_float.cpp
@@ -162,6 +162,7 @@ FLOAT_TEST_EXPECT_SUCCESS(float_zero_dot_suffix_L,      "0.L");
 FLOAT_TEST_EXPECT_SUCCESS(float_dot_zero_suffix_f,      ".0f");
 FLOAT_TEST_EXPECT_SUCCESS(float_dot_zero_suffix_F,      ".0F");
 FLOAT_TEST_EXPECT_SUCCESS(float_dot_zero_suffix_l,      ".0l");
+FLOAT_TEST_EXPECT_SUCCESS(float_dot_zero_suffix_L,      ".0L");
 
 #define expect_digit ERROR_LIBCPARSE_PP_SCANNER_EXPECTING_DIGIT
 #define expect_p     ERROR_LIBCPARSE_PP_SCANNER_HEX_FLOAT_EXPECTING_P
