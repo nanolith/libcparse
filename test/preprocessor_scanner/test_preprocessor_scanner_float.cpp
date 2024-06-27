cada3ea3ada69d19c167184c6f8064fd62393401
Justin Handville <nanolith@gmail.com>
2024-06-26 20:19:22 -0400

    1.4e+7l unit test.

--- test/preprocessor_scanner/test_preprocessor_scanner_float.cpp
+++ test/preprocessor_scanner/test_preprocessor_scanner_float.cpp
@@ -169,6 +169,7 @@ FLOAT_TEST_EXPECT_SUCCESS(float_1_dot_4e7_suffix_l,     "1.4e7l");
 FLOAT_TEST_EXPECT_SUCCESS(float_1_dot_4e7_suffix_L,     "1.4e7L");
 FLOAT_TEST_EXPECT_SUCCESS(float_1_dot_4e_p_7_suffix_f,  "1.4e+7f");
 FLOAT_TEST_EXPECT_SUCCESS(float_1_dot_4e_p_7_suffix_F,  "1.4e+7F");
+FLOAT_TEST_EXPECT_SUCCESS(float_1_dot_4e_p_7_suffix_l,  "1.4e+7l");
 
 #define expect_digit ERROR_LIBCPARSE_PP_SCANNER_EXPECTING_DIGIT
 #define expect_p     ERROR_LIBCPARSE_PP_SCANNER_HEX_FLOAT_EXPECTING_P
