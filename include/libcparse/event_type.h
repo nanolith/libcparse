bb5ff65849556dc9ad05d38c643338bf3b8220c9
Justin Handville <nanolith@gmail.com>
2024-07-01 21:37:27 -0400

    _Alignof type end exp event type decl.

--- include/libcparse/event_type.h
+++ include/libcparse/event_type.h
@@ -219,6 +219,7 @@ enum CPARSE_SYM(event_type)
     CPARSE_EVENT_TYPE_EXP_SIZEOF_TYPE_BEGIN =                           0x0228,
     CPARSE_EVENT_TYPE_EXP_SIZEOF_TYPE_END =                             0x0229,
     CPARSE_EVENT_TYPE_EXP_ALIGNOF_TYPE_BEGIN =                          0x022A,
+    CPARSE_EVENT_TYPE_EXP_ALIGNOF_TYPE_END =                            0x022B,
 
     CPARSE_EVENT_TYPE_UNKNOWN =                                         0xFFFF,
 };
