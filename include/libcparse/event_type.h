fdefe78048d85355db4796210afe4c81513b46aa
Justin Handville <nanolith@gmail.com>
2024-07-02 23:07:34 -0400

    Sub begin exp event type decl.

--- include/libcparse/event_type.h
+++ include/libcparse/event_type.h
@@ -236,6 +236,7 @@ enum CPARSE_SYM(event_type)
     /* additive expressions. */
     CPARSE_EVENT_TYPE_EXP_ADD_BEGIN =                                   0x0250,
     CPARSE_EVENT_TYPE_EXP_ADD_END =                                     0x0251,
+    CPARSE_EVENT_TYPE_EXP_SUB_END =                                     0x0252,
 
     CPARSE_EVENT_TYPE_UNKNOWN =                                         0xFFFF,
 };
