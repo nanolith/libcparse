470063020c50f58c8b2a610529d369417a767bae
Justin Handville <nanolith@gmail.com>
2024-07-09 21:51:16 -0400

    event_init_for_exp_dot_member_deref_begin export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -2183,6 +2183,10 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return \
                 CPARSE_SYM(event_init_for_exp_function_call_end)(x,y); } \
+    static inline int sym ## event_init_for_exp_dot_member_deref_begin( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return \
+                CPARSE_SYM(event_init_for_exp_dot_member_deref_begin)(x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
