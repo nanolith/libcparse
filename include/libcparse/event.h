76f81fb8fc13ef85c93abdc633e522a19cbfad99
Justin Handville <nanolith@gmail.com>
2024-07-10 23:04:52 -0400

    event_init_for_exp_postfix_decrement_begin export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -2293,6 +2293,10 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return \
                 CPARSE_SYM(event_init_for_exp_postfix_increment_end)(x,y); } \
+    static inline int sym ## event_init_for_exp_postfix_decrement_begin( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return \
+                CPARSE_SYM(event_init_for_exp_postfix_decrement_begin)(x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
