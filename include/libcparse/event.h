40d08edd8abeaedf19b6dabcfdd7d1d7384f721a
Justin Handville <nanolith@gmail.com>
2024-07-12 23:08:44 -0400

    event_init_for_exp_unary_operation_begin decl.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -1932,6 +1932,20 @@ int CPARSE_SYM(event_init_for_exp_prefix_decrement_end)(
     CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
 
 /**
+ * \brief Perform an in-place initialization of a unary operation begin
+ * expression event instance.
+ *
+ * \param ev                    Pointer to the event to initialize.
+ * \param cursor                The event cursor.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(event_init_for_exp_unary_operation_begin)(
+    CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
+
+/**
  * \brief Perform an in-place disposal of an event instance.
  *
  * \param ev                    Pointer to the event to dispose.
