7117345edb3827ca3bc47be8b02650b01ad40ae2
Justin Handville <nanolith@gmail.com>
2024-07-13 23:16:38 -0400

    event_init_for_exp_alignof_type_begin export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -2559,6 +2559,10 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return \
                 CPARSE_SYM(event_init_for_exp_sizeof_type_end)(x,y); } \
+    static inline int sym ## event_init_for_exp_alignof_type_begin( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return \
+                CPARSE_SYM(event_init_for_exp_alignof_type_begin)(x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
