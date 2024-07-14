d94efc226b327b65dbb4111a37fc377e0d3de4f2
Justin Handville <nanolith@gmail.com>
2024-07-13 23:12:00 -0400

    event_init_for_exp_sizeof_type_begin export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -2523,6 +2523,10 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return \
                 CPARSE_SYM(event_init_for_exp_sizeof_exp_end)(x,y); } \
+    static inline int sym ## event_init_for_exp_sizeof_type_begin( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return \
+                CPARSE_SYM(event_init_for_exp_sizeof_type_begin)(x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
