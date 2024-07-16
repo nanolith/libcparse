aec1dc4458abb054726229d6493b9097fe3ee543
Justin Handville <nanolith@gmail.com>
2024-07-15 23:07:23 -0400

    event_init_for_exp_add_end export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -2742,6 +2742,9 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
     static inline int sym ## event_init_for_exp_add_begin( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_exp_add_begin)(x,y); } \
+    static inline int sym ## event_init_for_exp_add_end( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return CPARSE_SYM(event_init_for_exp_add_end)(x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
