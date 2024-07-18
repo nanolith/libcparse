63a40fe0d4e7720c7ac91bc5831ffc0c51a2d1cf
Justin Handville <nanolith@gmail.com>
2024-07-17 23:00:45 -0400

    event_init_for_exp_less_than_equal_begin decl.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -2324,6 +2324,20 @@ int CPARSE_SYM(event_init_for_exp_greater_than_end)(
     CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
 
 /**
+ * \brief Perform an in-place initialization of a less-than equal begin
+ * expression event instance.
+ *
+ * \param ev                    Pointer to the event to initialize.
+ * \param cursor                The event cursor.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int CPARSE_SYM(event_init_for_exp_less_than_equal_begin)(
+    CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
+
+/**
  * \brief Perform an in-place disposal of an event instance.
  *
  * \param ev                    Pointer to the event to dispose.
