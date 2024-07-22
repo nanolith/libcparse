23fb4a225608bf7c2236ae3ae8cd1f021254782f
Justin Handville <nanolith@gmail.com>
2024-07-21 22:34:01 -0400

    event_init_for_exp_conditional_begin export.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -3254,6 +3254,9 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
     static inline int sym ## event_init_for_exp_logical_or_end( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_exp_logical_or_end)(x,y); } \
+    static inline int sym ## event_init_for_exp_conditional_begin( \
+        CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
+            return CPARSE_SYM(event_init_for_exp_conditional_begin)(x,y); } \
     static inline int sym ## event_dispose( \
         CPARSE_SYM(event)* x) { \
             return CPARSE_SYM(event_dispose)(x); } \
