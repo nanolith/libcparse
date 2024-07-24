ccc08d2c53bd3071420da02c6dd5c5fa9df457d7
Justin Handville <nanolith@gmail.com>
2024-07-23 21:53:40 -0400

    event_init_for_token_right_paren DRY unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -50,27 +50,8 @@ EVENT_INIT_TYPE_TEST(
     event_init_for_comment_line_end, CPARSE_EVENT_TYPE_COMMENT_LINE_END);
 EVENT_INIT_TYPE_TEST(
     event_init_for_token_left_paren, CPARSE_EVENT_TYPE_TOKEN_LEFT_PAREN);
-
-/**
- * Test that we can create a token right paren event.
- */
-TEST(token_right_paren)
-{
-    event ev;
-    cursor c;
-
-    /* clear the cursor. */
-    memset(&c, 0, sizeof(c));
-
-    /* Initialize an event. */
-    TEST_ASSERT(STATUS_SUCCESS == event_init_for_token_right_paren(&ev, &c));
-
-    /* The event type is correct. */
-    TEST_EXPECT(CPARSE_EVENT_TYPE_TOKEN_RIGHT_PAREN == event_get_type(&ev));
-
-    /* clean up. */
-    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
-}
+EVENT_INIT_TYPE_TEST(
+    event_init_for_token_right_paren, CPARSE_EVENT_TYPE_TOKEN_RIGHT_PAREN);
 
 /**
  * Test that we can create a token left brace event.
