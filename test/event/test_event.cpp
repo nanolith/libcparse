dd55e6516b42db9863527ae78400bcf84252ea75
Justin Handville <nanolith@gmail.com>
2024-07-25 22:01:59 -0400

    event_init_for_token_question DRY unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -82,27 +82,8 @@ EVENT_INIT_TYPE_TEST(
 EVENT_INIT_TYPE_TEST(event_init_for_token_pipe, CPARSE_EVENT_TYPE_TOKEN_PIPE);
 EVENT_INIT_TYPE_TEST(event_init_for_token_caret, CPARSE_EVENT_TYPE_TOKEN_CARET);
 EVENT_INIT_TYPE_TEST(event_init_for_token_tilde, CPARSE_EVENT_TYPE_TOKEN_TILDE);
-
-/**
- * Test that we can create a token question event.
- */
-TEST(token_question)
-{
-    event ev;
-    cursor c;
-
-    /* clear the cursor. */
-    memset(&c, 0, sizeof(c));
-
-    /* Initialize an event. */
-    TEST_ASSERT(STATUS_SUCCESS == event_init_for_token_question(&ev, &c));
-
-    /* The event type is correct. */
-    TEST_EXPECT(CPARSE_EVENT_TYPE_TOKEN_QUESTION == event_get_type(&ev));
-
-    /* clean up. */
-    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
-}
+EVENT_INIT_TYPE_TEST(
+    event_init_for_token_question, CPARSE_EVENT_TYPE_TOKEN_QUESTION);
 
 /**
  * Test that we can create a token equal compare event.
