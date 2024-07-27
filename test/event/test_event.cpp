32d9d3036b31655aa0970ae6abae9c94071728d8
Justin Handville <nanolith@gmail.com>
2024-07-26 22:51:13 -0400

    event_init_for_token_equal_assign DRY unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -89,29 +89,8 @@ EVENT_INIT_TYPE_TEST(
 EVENT_INIT_TYPE_TEST(
     event_init_for_token_not_equal_compare,
     CPARSE_EVENT_TYPE_TOKEN_NOT_EQUAL_COMPARE);
-
-/**
- * Test that we can create a token equal assign event.
- */
-TEST(token_equal_assign)
-{
-    event ev;
-    cursor c;
-
-    /* clear the cursor. */
-    memset(&c, 0, sizeof(c));
-
-    /* Initialize an event. */
-    TEST_ASSERT(
-        STATUS_SUCCESS == event_init_for_token_equal_assign(&ev, &c));
-
-    /* The event type is correct. */
-    TEST_EXPECT(
-        CPARSE_EVENT_TYPE_TOKEN_EQUAL_ASSIGN == event_get_type(&ev));
-
-    /* clean up. */
-    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
-}
+EVENT_INIT_TYPE_TEST(
+    event_init_for_token_equal_assign, CPARSE_EVENT_TYPE_TOKEN_EQUAL_ASSIGN);
 
 /**
  * Test that we can create a token plus equal event.
