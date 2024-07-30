951d6aa7355af192d30524350e179784694be0a9
Justin Handville <nanolith@gmail.com>
2024-07-29 22:38:37 -0400

    event_init_for_token_keyword__Noreturn DRY unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -165,29 +165,9 @@ EVENT_INIT_TYPE_TEST(
 EVENT_INIT_TYPE_TEST(
     event_init_for_token_keyword__Imaginary,
     CPARSE_EVENT_TYPE_TOKEN_KEYWORD__IMAGINARY);
-
-/**
- * Test that we can create a _Noreturn keyword token event.
- */
-TEST(_Noreturn_keyword_token)
-{
-    event ev;
-    cursor c;
-
-    /* clear the cursor. */
-    memset(&c, 0, sizeof(c));
-
-    /* Initialize an event. */
-    TEST_ASSERT(
-        STATUS_SUCCESS == event_init_for_token_keyword__Noreturn(&ev, &c));
-
-    /* The event type is correct. */
-    TEST_EXPECT(
-        CPARSE_EVENT_TYPE_TOKEN_KEYWORD__NORETURN == event_get_type(&ev));
-
-    /* clean up. */
-    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
-}
+EVENT_INIT_TYPE_TEST(
+    event_init_for_token_keyword__Noreturn,
+    CPARSE_EVENT_TYPE_TOKEN_KEYWORD__NORETURN);
 
 /**
  * Test that we can create a _Static_assert keyword token event.
