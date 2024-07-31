bf15e93abca2204cf0aeccf97ce1ce02ef53ec33
Justin Handville <nanolith@gmail.com>
2024-07-30 22:01:30 -0400

    event_init_for_token_keyword_default DRY unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -187,29 +187,9 @@ EVENT_INIT_TYPE_TEST(
 EVENT_INIT_TYPE_TEST(
     event_init_for_token_keyword_continue,
     CPARSE_EVENT_TYPE_TOKEN_KEYWORD_CONTINUE);
-
-/**
- * Test that we can create a default keyword token event.
- */
-TEST(default_keyword_token)
-{
-    event ev;
-    cursor c;
-
-    /* clear the cursor. */
-    memset(&c, 0, sizeof(c));
-
-    /* Initialize an event. */
-    TEST_ASSERT(
-        STATUS_SUCCESS == event_init_for_token_keyword_default(&ev, &c));
-
-    /* The event type is correct. */
-    TEST_EXPECT(
-        CPARSE_EVENT_TYPE_TOKEN_KEYWORD_DEFAULT == event_get_type(&ev));
-
-    /* clean up. */
-    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
-}
+EVENT_INIT_TYPE_TEST(
+    event_init_for_token_keyword_default,
+    CPARSE_EVENT_TYPE_TOKEN_KEYWORD_DEFAULT);
 
 /**
  * Test that we can create a do keyword token event.
