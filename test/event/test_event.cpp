91692a3d516a0319221666b678b7247709fa1965
Justin Handville <nanolith@gmail.com>
2024-07-31 23:15:46 -0400

    event_init_for_token_keyword_long DRY unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -215,29 +215,8 @@ EVENT_INIT_TYPE_TEST(
     CPARSE_EVENT_TYPE_TOKEN_KEYWORD_INLINE);
 EVENT_INIT_TYPE_TEST(
     event_init_for_token_keyword_int, CPARSE_EVENT_TYPE_TOKEN_KEYWORD_INT);
-
-/**
- * Test that we can create a long keyword token event.
- */
-TEST(long_keyword_token)
-{
-    event ev;
-    cursor c;
-
-    /* clear the cursor. */
-    memset(&c, 0, sizeof(c));
-
-    /* Initialize an event. */
-    TEST_ASSERT(
-        STATUS_SUCCESS == event_init_for_token_keyword_long(&ev, &c));
-
-    /* The event type is correct. */
-    TEST_EXPECT(
-        CPARSE_EVENT_TYPE_TOKEN_KEYWORD_LONG == event_get_type(&ev));
-
-    /* clean up. */
-    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
-}
+EVENT_INIT_TYPE_TEST(
+    event_init_for_token_keyword_long, CPARSE_EVENT_TYPE_TOKEN_KEYWORD_LONG);
 
 /**
  * Test that we can create a register keyword token event.
