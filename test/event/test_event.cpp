fffcdb05604ce0b18bb0f6a345191cf335982bca
Justin Handville <nanolith@gmail.com>
2024-08-01 22:29:10 -0400

    event_init_for_token_keyword_unsigned DRY unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -248,29 +248,9 @@ EVENT_INIT_TYPE_TEST(
     CPARSE_EVENT_TYPE_TOKEN_KEYWORD_TYPEDEF);
 EVENT_INIT_TYPE_TEST(
     event_init_for_token_keyword_union, CPARSE_EVENT_TYPE_TOKEN_KEYWORD_UNION);
-
-/**
- * Test that we can create a unsigned keyword token event.
- */
-TEST(unsigned_keyword_token)
-{
-    event ev;
-    cursor c;
-
-    /* clear the cursor. */
-    memset(&c, 0, sizeof(c));
-
-    /* Initialize an event. */
-    TEST_ASSERT(
-        STATUS_SUCCESS == event_init_for_token_keyword_unsigned(&ev, &c));
-
-    /* The event type is correct. */
-    TEST_EXPECT(
-        CPARSE_EVENT_TYPE_TOKEN_KEYWORD_UNSIGNED == event_get_type(&ev));
-
-    /* clean up. */
-    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
-}
+EVENT_INIT_TYPE_TEST(
+    event_init_for_token_keyword_unsigned,
+    CPARSE_EVENT_TYPE_TOKEN_KEYWORD_UNSIGNED);
 
 /**
  * Test that we can create a void keyword token event.
