19e28511b61b73d24bd2ef4d448da4a80c610bac
Justin Handville <nanolith@gmail.com>
2024-08-01 22:29:59 -0400

    event_init_for_token_keyword_void DRY unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -251,29 +251,8 @@ EVENT_INIT_TYPE_TEST(
 EVENT_INIT_TYPE_TEST(
     event_init_for_token_keyword_unsigned,
     CPARSE_EVENT_TYPE_TOKEN_KEYWORD_UNSIGNED);
-
-/**
- * Test that we can create a void keyword token event.
- */
-TEST(void_keyword_token)
-{
-    event ev;
-    cursor c;
-
-    /* clear the cursor. */
-    memset(&c, 0, sizeof(c));
-
-    /* Initialize an event. */
-    TEST_ASSERT(
-        STATUS_SUCCESS == event_init_for_token_keyword_void(&ev, &c));
-
-    /* The event type is correct. */
-    TEST_EXPECT(
-        CPARSE_EVENT_TYPE_TOKEN_KEYWORD_VOID == event_get_type(&ev));
-
-    /* clean up. */
-    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
-}
+EVENT_INIT_TYPE_TEST(
+    event_init_for_token_keyword_void, CPARSE_EVENT_TYPE_TOKEN_KEYWORD_VOID);
 
 /**
  * Test that we can create a volatile keyword token event.
