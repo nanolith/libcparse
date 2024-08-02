be2b027cc47101b5423f51693d96941544cfda07
Justin Handville <nanolith@gmail.com>
2024-08-01 22:26:29 -0400

    event_init_for_token_keyword_switch DRY unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -240,29 +240,9 @@ EVENT_INIT_TYPE_TEST(
 EVENT_INIT_TYPE_TEST(
     event_init_for_token_keyword_struct,
     CPARSE_EVENT_TYPE_TOKEN_KEYWORD_STRUCT);
-
-/**
- * Test that we can create a switch keyword token event.
- */
-TEST(switch_keyword_token)
-{
-    event ev;
-    cursor c;
-
-    /* clear the cursor. */
-    memset(&c, 0, sizeof(c));
-
-    /* Initialize an event. */
-    TEST_ASSERT(
-        STATUS_SUCCESS == event_init_for_token_keyword_switch(&ev, &c));
-
-    /* The event type is correct. */
-    TEST_EXPECT(
-        CPARSE_EVENT_TYPE_TOKEN_KEYWORD_SWITCH == event_get_type(&ev));
-
-    /* clean up. */
-    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
-}
+EVENT_INIT_TYPE_TEST(
+    event_init_for_token_keyword_switch,
+    CPARSE_EVENT_TYPE_TOKEN_KEYWORD_SWITCH);
 
 /**
  * Test that we can create a typedef keyword token event.
