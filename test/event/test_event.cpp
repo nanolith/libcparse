5f23e5f9e962c7b3d562b0210e0a3487ec895ba6
Justin Handville <nanolith@gmail.com>
2024-08-01 22:28:19 -0400

    event_init_for_token_keyword_union DRY unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -246,29 +246,8 @@ EVENT_INIT_TYPE_TEST(
 EVENT_INIT_TYPE_TEST(
     event_init_for_token_keyword_typedef,
     CPARSE_EVENT_TYPE_TOKEN_KEYWORD_TYPEDEF);
-
-/**
- * Test that we can create a union keyword token event.
- */
-TEST(union_keyword_token)
-{
-    event ev;
-    cursor c;
-
-    /* clear the cursor. */
-    memset(&c, 0, sizeof(c));
-
-    /* Initialize an event. */
-    TEST_ASSERT(
-        STATUS_SUCCESS == event_init_for_token_keyword_union(&ev, &c));
-
-    /* The event type is correct. */
-    TEST_EXPECT(
-        CPARSE_EVENT_TYPE_TOKEN_KEYWORD_UNION == event_get_type(&ev));
-
-    /* clean up. */
-    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
-}
+EVENT_INIT_TYPE_TEST(
+    event_init_for_token_keyword_union, CPARSE_EVENT_TYPE_TOKEN_KEYWORD_UNION);
 
 /**
  * Test that we can create a unsigned keyword token event.
