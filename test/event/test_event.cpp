b06a6a36e32d381c4e90dd47c75d2d6eb251bcdb
Justin Handville <nanolith@gmail.com>
2024-08-13 22:13:29 -0400

    event_init_for_exp_equal_to_begin unit test.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -407,3 +407,5 @@ EVENT_INIT_TYPE_TEST(
 EVENT_INIT_TYPE_TEST(
     event_init_for_exp_greater_than_equal_end,
     CPARSE_EVENT_TYPE_EXP_GREATER_THAN_EQUAL_END);
+EVENT_INIT_TYPE_TEST(
+    event_init_for_exp_equal_to_begin, CPARSE_EVENT_TYPE_EXP_EQUAL_TO_BEGIN);
