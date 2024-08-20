197812f50fa33ffcc009c6f1dc83a487bba303bd
Justin Handville <nanolith@gmail.com>
2024-08-19 22:34:20 -0400

    CPARSE_EVENT_CATEGORY_RAW_CHARACTER_LITERAL decl.

--- src/event/event_internal.h
+++ src/event/event_internal.h
@@ -27,6 +27,7 @@ enum CPARSE_SYM(event_category)
     CPARSE_EVENT_CATEGORY_INCLUDE =                                     0x0002,
     CPARSE_EVENT_CATEGORY_INTEGER_TOKEN =                               0x0003,
     CPARSE_EVENT_CATEGORY_RAW_CHARACTER =                               0x0004,
+    CPARSE_EVENT_CATEGORY_RAW_CHARACTER_LITERAL =                       0x0005,
 };
 
 /******************************************************************************/
