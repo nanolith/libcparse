db12b0b60808bc286972d5a69773554a87071c7d
Justin Handville <nanolith@gmail.com>
2024-08-26 21:50:51 -0400

    event_init_for_token_preprocessor_id_if category UT.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -217,8 +217,9 @@ EVENT_INIT_CAT_TYPE_TEST(
 EVENT_INIT_CAT_TYPE_TEST(
     event_init_for_token_ellipsis, CPARSE_EVENT_CATEGORY_BASE,
     CPARSE_EVENT_TYPE_TOKEN_ELLIPSIS);
-EVENT_INIT_TYPE_TEST(
-    event_init_for_token_preprocessor_id_if, CPARSE_EVENT_TYPE_TOKEN_PP_ID_IF);
+EVENT_INIT_CAT_TYPE_TEST(
+    event_init_for_token_preprocessor_id_if, CPARSE_EVENT_CATEGORY_BASE,
+    CPARSE_EVENT_TYPE_TOKEN_PP_ID_IF);
 EVENT_INIT_TYPE_TEST(
     event_init_for_token_preprocessor_id_ifdef,
     CPARSE_EVENT_TYPE_TOKEN_PP_ID_IFDEF);
