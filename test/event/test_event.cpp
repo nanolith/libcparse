bdc8c679308529ed3c8353db540bc63c8b2571ad
Justin Handville <nanolith@gmail.com>
2024-09-01 22:19:40 -0400

    event_init_for_exp_function_call_end category UT.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -394,8 +394,8 @@ EVENT_INIT_CAT_TYPE_TEST(
 EVENT_INIT_CAT_TYPE_TEST(
     event_init_for_exp_function_call_begin, CPARSE_EVENT_CATEGORY_BASE,
     CPARSE_EVENT_TYPE_EXP_FUNCTION_CALL_BEGIN);
-EVENT_INIT_TYPE_TEST(
-    event_init_for_exp_function_call_end,
+EVENT_INIT_CAT_TYPE_TEST(
+    event_init_for_exp_function_call_end, CPARSE_EVENT_CATEGORY_BASE,
     CPARSE_EVENT_TYPE_EXP_FUNCTION_CALL_END);
 EVENT_INIT_TYPE_TEST(
     event_init_for_exp_dot_member_deref_begin,
