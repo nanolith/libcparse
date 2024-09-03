7e0589fbe5fcd0d86b8eba5d851f7bc41d215404
Justin Handville <nanolith@gmail.com>
2024-09-02 22:52:14 -0400

    event_init_for_exp_prefix_increment_begin category UT.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -427,8 +427,8 @@ EVENT_INIT_CAT_TYPE_TEST(
 EVENT_INIT_CAT_TYPE_TEST(
     event_init_for_exp_compound_literal_end, CPARSE_EVENT_CATEGORY_BASE,
     CPARSE_EVENT_TYPE_EXP_COMPOUND_LITERAL_END);
-EVENT_INIT_TYPE_TEST(
-    event_init_for_exp_prefix_increment_begin,
+EVENT_INIT_CAT_TYPE_TEST(
+    event_init_for_exp_prefix_increment_begin, CPARSE_EVENT_CATEGORY_BASE,
     CPARSE_EVENT_TYPE_EXP_PREFIX_INCREMENT_BEGIN);
 EVENT_INIT_TYPE_TEST(
     event_init_for_exp_prefix_increment_end,
