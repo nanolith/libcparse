c3cac5c60e8092cf3cac13f353dc80db28b62c19
Justin Handville <nanolith@gmail.com>
2024-09-05 20:57:50 -0400

    event_init_for_exp_less_than_equal_end category UT.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -526,8 +526,8 @@ EVENT_INIT_CAT_TYPE_TEST(
 EVENT_INIT_CAT_TYPE_TEST(
     event_init_for_exp_less_than_equal_begin, CPARSE_EVENT_CATEGORY_BASE,
     CPARSE_EVENT_TYPE_EXP_LESS_THAN_EQUAL_BEGIN);
-EVENT_INIT_TYPE_TEST(
-    event_init_for_exp_less_than_equal_end,
+EVENT_INIT_CAT_TYPE_TEST(
+    event_init_for_exp_less_than_equal_end, CPARSE_EVENT_CATEGORY_BASE,
     CPARSE_EVENT_TYPE_EXP_LESS_THAN_EQUAL_END);
 EVENT_INIT_TYPE_TEST(
     event_init_for_exp_greater_than_equal_begin,
