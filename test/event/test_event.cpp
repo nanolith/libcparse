f0a3cfcbde9383a93a7b51a653bc3bcbb394cee6
Justin Handville <nanolith@gmail.com>
2024-09-06 21:38:36 -0400

    event_init_for_exp_bitwise_xor_begin category UT.

--- test/event/test_event.cpp
+++ test/event/test_event.cpp
@@ -553,8 +553,8 @@ EVENT_INIT_CAT_TYPE_TEST(
 EVENT_INIT_CAT_TYPE_TEST(
     event_init_for_exp_bitwise_and_end, CPARSE_EVENT_CATEGORY_BASE,
     CPARSE_EVENT_TYPE_EXP_BITWISE_AND_END);
-EVENT_INIT_TYPE_TEST(
-    event_init_for_exp_bitwise_xor_begin,
+EVENT_INIT_CAT_TYPE_TEST(
+    event_init_for_exp_bitwise_xor_begin, CPARSE_EVENT_CATEGORY_BASE,
     CPARSE_EVENT_TYPE_EXP_BITWISE_XOR_BEGIN);
 EVENT_INIT_TYPE_TEST(
     event_init_for_exp_bitwise_xor_end, CPARSE_EVENT_TYPE_EXP_BITWISE_XOR_END);
