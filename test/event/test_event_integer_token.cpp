1c76fcdce9e42bf85240c2a5f3dab70f01d196d7
Justin Handville <nanolith@gmail.com>
2024-09-11 23:26:22 -0400

    unsigned_integer_to_short unit test.

--- test/event/test_event_integer_token.cpp
+++ test/event/test_event_integer_token.cpp
@@ -504,3 +504,32 @@ TEST(signed_integer_to_short_positive_conversion_error)
     /* we can dispose the event. */
     TEST_ASSERT(STATUS_SUCCESS == event_integer_token_dispose(&ev));
 }
+
+/**
+ * Test that we can convert an unsigned integer token to a short.
+ */
+TEST(unsigned_integer_to_short)
+{
+    cursor pos;
+    event_integer_token ev;
+    const int TEST_VAL = 12;
+    short val = 0;
+
+    memset(&pos, 0, sizeof(pos));
+
+    /* we can initialize the event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == event_integer_token_init_for_signed_int(
+                    &ev, &pos, TEST_VAL));
+
+    /* we can convert this value to a short. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_integer_token_convert_to_short(&val, &ev));
+
+    /* thi short value matches. */
+    TEST_EXPECT(TEST_VAL == val);
+
+    /* we can dispose the event. */
+    TEST_ASSERT(STATUS_SUCCESS == event_integer_token_dispose(&ev));
+}
