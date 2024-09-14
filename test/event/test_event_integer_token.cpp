c3dddd47f9ae26be8984cca8534a9e21d95caedb
Justin Handville <nanolith@gmail.com>
2024-09-13 23:08:18 -0400

    signed_long_long_to_unsigned_long_negative_conversion_error unit test.

--- test/event/test_event_integer_token.cpp
+++ test/event/test_event_integer_token.cpp
@@ -1070,3 +1070,30 @@ TEST(signed_long_long_to_unsigned_long)
     /* we can dispose the event. */
     TEST_ASSERT(STATUS_SUCCESS == event_integer_token_dispose(&ev));
 }
+
+/**
+ * We get a conversion error if the signed value is < 0.
+ */
+TEST(signed_long_long_to_unsigned_long_negative_conversion_error)
+{
+    cursor pos;
+    event_integer_token ev;
+    const long long TEST_VAL = -5;
+    unsigned long val = 0;
+
+    memset(&pos, 0, sizeof(pos));
+
+    /* we can initialize the event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == event_integer_token_init_for_signed_long_long(
+                    &ev, &pos, TEST_VAL));
+
+    /* conversion fails. */
+    TEST_ASSERT(
+        ERROR_LIBCPARSE_BAD_INTEGER_CONVERSION
+            == event_integer_token_convert_to_unsigned_long(&val, &ev));
+
+    /* we can dispose the event. */
+    TEST_ASSERT(STATUS_SUCCESS == event_integer_token_dispose(&ev));
+}
