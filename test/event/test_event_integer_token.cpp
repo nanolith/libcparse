c649f199fb7d8938316435ac3ca80b83a9719e94
Justin Handville <nanolith@gmail.com>
2024-09-15 21:22:25 -0400

    unsigned_integer_to_uint8 unit test.

--- test/event/test_event_integer_token.cpp
+++ test/event/test_event_integer_token.cpp
@@ -1468,3 +1468,32 @@ TEST(signed_integer_to_uint8_positive_conversion_error)
     /* we can dispose the event. */
     TEST_ASSERT(STATUS_SUCCESS == event_integer_token_dispose(&ev));
 }
+
+/**
+ * Test that we can convert an unsigned integer token to a uint8.
+ */
+TEST(unsigned_integer_to_uint8)
+{
+    cursor pos;
+    event_integer_token ev;
+    const unsigned int TEST_VAL = 12;
+    uint8_t val = 0;
+
+    memset(&pos, 0, sizeof(pos));
+
+    /* we can initialize the event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == event_integer_token_init_for_unsigned_int(
+                    &ev, &pos, TEST_VAL));
+
+    /* we can convert this value to a uint8. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_integer_token_convert_to_uint8(&val, &ev));
+
+    /* the uint8 value matches. */
+    TEST_EXPECT(TEST_VAL == val);
+
+    /* we can dispose the event. */
+    TEST_ASSERT(STATUS_SUCCESS == event_integer_token_dispose(&ev));
+}
