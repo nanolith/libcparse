49910fe574830a5d72b90d4bd95e84312e02b76f
Justin Handville <nanolith@gmail.com>
2024-09-16 21:43:58 -0400

    unsigned_long_to_int32 unit test.

--- test/event/test_event_integer_token.cpp
+++ test/event/test_event_integer_token.cpp
@@ -1885,3 +1885,32 @@ TEST(signed_long_to_int32_positive_conversion_error)
     /* we can dispose the event. */
     TEST_ASSERT(STATUS_SUCCESS == event_integer_token_dispose(&ev));
 }
+
+/**
+ * Test that we can convert an unsigned long token to an int32.
+ */
+TEST(unsigned_long_to_int32)
+{
+    cursor pos;
+    event_integer_token ev;
+    const unsigned long TEST_VAL = 12;
+    int32_t val = 0;
+
+    memset(&pos, 0, sizeof(pos));
+
+    /* we can initialize the event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == event_integer_token_init_for_unsigned_long(
+                    &ev, &pos, TEST_VAL));
+
+    /* we can convert this value to an int32. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == event_integer_token_convert_to_int32(&val, &ev));
+
+    /* the int32 value matches. */
+    TEST_EXPECT(TEST_VAL == val);
+
+    /* we can dispose the event. */
+    TEST_ASSERT(STATUS_SUCCESS == event_integer_token_dispose(&ev));
+}
