54fcf104610c6c7f146ded806359828c9a894d6d
Justin Handville <nanolith@gmail.com>
2024-09-17 21:58:00 -0400

    signed_integer_coerce_to_char unit test.

--- test/event/test_event_integer_token.cpp
+++ test/event/test_event_integer_token.cpp
@@ -2196,3 +2196,31 @@ TEST(unsigned_long_to_uint64)
     /* we can dispose the event. */
     TEST_ASSERT(STATUS_SUCCESS == event_integer_token_dispose(&ev));
 }
+
+/**
+ * Test that we can coerce a signed integer to char.
+ */
+TEST(signed_integer_coerce_to_char)
+{
+    cursor pos;
+    event_integer_token ev;
+    const int TEST_VAL = 12;
+    char val = 0;
+
+    memset(&pos, 0, sizeof(pos));
+
+    /* we can initialize the event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == event_integer_token_init_for_signed_int(
+                    &ev, &pos, TEST_VAL));
+
+    /* we can convert this value to a char. */
+    val = event_integer_token_coerce_char(&ev);
+
+    /* the char value matches. */
+    TEST_EXPECT(TEST_VAL == val);
+
+    /* we can dispose the event. */
+    TEST_ASSERT(STATUS_SUCCESS == event_integer_token_dispose(&ev));
+}
