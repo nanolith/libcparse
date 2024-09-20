d1ed7b6ea6e77e849ee2f2f895260164dbc16306
Justin Handville <nanolith@gmail.com>
2024-09-19 22:10:10 -0400

    unsigned_integer_coerce_to_int8 unit test.

--- test/event/test_event_integer_token.cpp
+++ test/event/test_event_integer_token.cpp
@@ -2784,3 +2784,31 @@ TEST(signed_integer_coerce_to_int8)
     /* we can dispose the event. */
     TEST_ASSERT(STATUS_SUCCESS == event_integer_token_dispose(&ev));
 }
+
+/**
+ * Test that we can coerce an unsigned integer to an int8.
+ */
+TEST(unsigned_integer_coerce_to_int8)
+{
+    cursor pos;
+    event_integer_token ev;
+    const unsigned int TEST_VAL = 12;
+    int8_t val = 0;
+
+    memset(&pos, 0, sizeof(pos));
+
+    /* we can initialize the event. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == event_integer_token_init_for_unsigned_int(
+                    &ev, &pos, TEST_VAL));
+
+    /* we can convert this value to an int8_t. */
+    val = event_integer_token_coerce_int8(&ev);
+
+    /* the unsigned short value matches. */
+    TEST_EXPECT(TEST_VAL == val);
+
+    /* we can dispose the event. */
+    TEST_ASSERT(STATUS_SUCCESS == event_integer_token_dispose(&ev));
+}
