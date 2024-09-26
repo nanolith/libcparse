3c9487ce17da28ae99b78b3bff1ffc2f6cb8fc60
Justin Handville <nanolith@gmail.com>
2024-09-25 22:14:36 -0400

    event_init_for_eof event_copy unit test.

---
+++ test/event_copy/test_event_copy.cpp
@@ -0,0 +1,61 @@
+/**
+ * \file test/event_copy/test_event_copy.cpp
+ *
+ * \brief Tests for the \ref event_copy type.
+ *
+ * \copyright 2023 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <libcparse/event.h>
+#include <libcparse/event_copy.h>
+#include <libcparse/status_codes.h>
+#include <minunit/minunit.h>
+#include <string.h>
+
+CPARSE_IMPORT_cursor;
+CPARSE_IMPORT_event;
+CPARSE_IMPORT_event_copy;
+
+TEST_SUITE(event_copy);
+
+static const char* TESTFILE = "test.c";
+
+/**
+ * Test that we can copy an eof event.
+ */
+TEST(event_init_for_eof)
+{
+    event ev;
+    cursor c;
+    event_copy* cpy;
+    const event* clone;
+    const cursor* clone_c;
+
+    memset(&c, 0, sizeof(c));
+    c.begin_line = 23;
+    c.end_line = 24;
+    c.begin_col = 1;
+    c.end_col = 5;
+    c.file = TESTFILE;
+
+    TEST_ASSERT(STATUS_SUCCESS == event_init_for_eof(&ev, &c));
+    TEST_ASSERT(STATUS_SUCCESS == event_copy_create(&cpy, &ev));
+
+    clone = event_copy_get_event(cpy);
+    TEST_ASSERT(NULL != clone);
+
+    TEST_EXPECT(event_get_type(&ev) == event_get_type(clone));
+
+    clone_c = event_get_cursor(clone);
+    TEST_ASSERT(NULL != clone_c);
+    TEST_EXPECT(clone_c->begin_line == c.begin_line);
+    TEST_EXPECT(clone_c->end_line == c.end_line);
+    TEST_EXPECT(clone_c->begin_col == c.begin_col);
+    TEST_EXPECT(clone_c->end_col == c.end_col);
+    TEST_ASSERT(NULL != clone_c->file);
+    TEST_EXPECT(!strcmp(c.file, clone_c->file));
+
+    TEST_ASSERT(STATUS_SUCCESS == event_copy_release(cpy));
+    TEST_ASSERT(STATUS_SUCCESS == event_dispose(&ev));
+}
