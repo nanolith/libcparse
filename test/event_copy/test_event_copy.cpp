3cfde888a79beda1325f4fba08a7e48580d883f9
Justin Handville <nanolith@gmail.com>
2024-10-03 22:23:53 -0400

    event_copy event_init_for_token_keyword_auto UT.

--- test/event_copy/test_event_copy.cpp
+++ test/event_copy/test_event_copy.cpp
@@ -136,3 +136,4 @@ EVENT_BASE_COPY_TEST(event_init_for_token_keyword__Imaginary);
 EVENT_BASE_COPY_TEST(event_init_for_token_keyword__Noreturn);
 EVENT_BASE_COPY_TEST(event_init_for_token_keyword__Static_assert);
 EVENT_BASE_COPY_TEST(event_init_for_token_keyword__Thread_local);
+EVENT_BASE_COPY_TEST(event_init_for_token_keyword_auto);
