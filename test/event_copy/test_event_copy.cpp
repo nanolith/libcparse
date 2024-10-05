41970b78d7d9b5e582d2c4cc5d76f6501d08f322
Justin Handville <nanolith@gmail.com>
2024-10-04 22:20:11 -0400

    event_copy event_init_for_token_keyword_for UT.

--- test/event_copy/test_event_copy.cpp
+++ test/event_copy/test_event_copy.cpp
@@ -149,3 +149,4 @@ EVENT_BASE_COPY_TEST(event_init_for_token_keyword_else);
 EVENT_BASE_COPY_TEST(event_init_for_token_keyword_enum);
 EVENT_BASE_COPY_TEST(event_init_for_token_keyword_extern);
 EVENT_BASE_COPY_TEST(event_init_for_token_keyword_float);
+EVENT_BASE_COPY_TEST(event_init_for_token_keyword_for);
