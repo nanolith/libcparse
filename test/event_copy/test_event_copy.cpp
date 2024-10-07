8963116223cf051ff06d67f7dcd2afa23c2e0290
Justin Handville <nanolith@gmail.com>
2024-10-06 23:11:03 -0400

    event_copy event_init_for_expression_end UT.

--- test/event_copy/test_event_copy.cpp
+++ test/event_copy/test_event_copy.cpp
@@ -172,3 +172,4 @@ EVENT_BASE_COPY_TEST(event_init_for_token_keyword_volatile);
 EVENT_BASE_COPY_TEST(event_init_for_token_keyword_while);
 EVENT_BASE_COPY_TEST(event_init_for_preprocessor_directive_end);
 EVENT_BASE_COPY_TEST(event_init_for_expression_begin);
+EVENT_BASE_COPY_TEST(event_init_for_expression_end);
