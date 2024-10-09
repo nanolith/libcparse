963db1270ef998f30326fccabc2b0db3dadabb0f
Justin Handville <nanolith@gmail.com>
2024-10-09 15:42:11 -0400

    event_copy event_init_for_exp_cast_end UT.

--- test/event_copy/test_event_copy.cpp
+++ test/event_copy/test_event_copy.cpp
@@ -204,3 +204,4 @@ EVENT_BASE_COPY_TEST(event_init_for_exp_sizeof_type_end);
 EVENT_BASE_COPY_TEST(event_init_for_exp_alignof_type_begin);
 EVENT_BASE_COPY_TEST(event_init_for_exp_alignof_type_end);
 EVENT_BASE_COPY_TEST(event_init_for_exp_cast_begin);
+EVENT_BASE_COPY_TEST(event_init_for_exp_cast_end);
