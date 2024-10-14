cdcf50dbe5886f22df80e321cd02b22572a7701c
Justin Handville <nanolith@gmail.com>
2024-10-13 22:30:24 -0400

    event_copy event_init_for_exp_conditional_begin UT.

--- test/event_copy/test_event_copy.cpp
+++ test/event_copy/test_event_copy.cpp
@@ -241,3 +241,4 @@ EVENT_BASE_COPY_TEST(event_init_for_exp_logical_and_begin);
 EVENT_BASE_COPY_TEST(event_init_for_exp_logical_and_end);
 EVENT_BASE_COPY_TEST(event_init_for_exp_logical_or_begin);
 EVENT_BASE_COPY_TEST(event_init_for_exp_logical_or_end);
+EVENT_BASE_COPY_TEST(event_init_for_exp_conditional_begin);
