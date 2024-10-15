5751d169ef6d28567282701b6a7ae240a20b843b
Justin Handville <nanolith@gmail.com>
2024-10-14 21:17:14 -0400

    Create an event_identifier copy.

--- src/event_copy/event_copy_create.c
+++ src/event_copy/event_copy_create.c
@@ -8,6 +8,7 @@
  */
 
 #include <libcparse/event_copy.h>
+#include <libcparse/event/identifier.h>
 #include <libcparse/status_codes.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,9 +19,11 @@
 CPARSE_IMPORT_cursor;
 CPARSE_IMPORT_event;
 CPARSE_IMPORT_event_copy;
+CPARSE_IMPORT_event_identifier;
 CPARSE_IMPORT_event_internal;
 
 static int event_copy_create_base(event_copy** cpy, const event* ev);
+static int event_copy_create_identifier(event_copy** cpy, const event* ev);
 static int event_copy_create_internal(
     event_copy** cpy, int category, const char* field, const cursor* cursor);
 
@@ -42,6 +45,9 @@ int CPARSE_SYM(event_copy_create)(
     {
         case CPARSE_EVENT_CATEGORY_BASE:
             return event_copy_create_base(cpy, ev);
+
+        case CPARSE_EVENT_CATEGORY_IDENTIFIER:
+            return event_copy_create_identifier(cpy, ev);
 
         default:
             return ERROR_LIBCPARSE_EVENT_COPY_UNSUPPORTED_EVENT_CATEGORY;
@@ -90,7 +96,67 @@ static int event_copy_create_base(event_copy** cpy, const event* ev)
     *cpy = tmp;
     retval = STATUS_SUCCESS;
     goto done;
-    
+
+cleanup_tmp:
+    release_retval = event_copy_release(tmp);
+    if (STATUS_SUCCESS != release_retval)
+    {
+        retval = release_retval;
+    }
+
+done:
+    return retval;
+}
+
+/**
+ * \brief Copy an identifier event.
+ *
+ * \param cpy                   Pointer to the \ref event_copy pointer to
+ *                              receive this \ref event_copy on success.
+ * \param ev                    The event to copy.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int event_copy_create_identifier(event_copy** cpy, const event* ev)
+{
+    event_copy* tmp = NULL;
+    event_identifier* iev;
+    int retval, release_retval;
+
+    /* downcast the event. */
+    retval = event_downcast_to_event_identifier(&iev, (event*)ev);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* create the copy. */
+    retval =
+        event_copy_create_internal(
+            &tmp, CPARSE_EVENT_CATEGORY_IDENTIFIER, event_identifier_get(iev),
+            event_get_cursor(ev));
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* initialize the identifier event. */
+    retval =
+        event_identifier_init(
+            &(tmp->detail.event_identifier), &(tmp->cursor), tmp->field1);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto cleanup_tmp;
+    }
+
+    /* success. */
+    tmp->initialized = true;
+    *cpy = tmp;
+    retval = STATUS_SUCCESS;
+    goto done;
+
 cleanup_tmp:
     release_retval = event_copy_release(tmp);
     if (STATUS_SUCCESS != release_retval)
