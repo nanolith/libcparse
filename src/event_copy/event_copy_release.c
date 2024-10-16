6bff3c748242fcd56d836e41caf15258eaa65d4b
Justin Handville <nanolith@gmail.com>
2024-10-15 23:14:53 -0400

    release integer events.

--- src/event_copy/event_copy_release.c
+++ src/event_copy/event_copy_release.c
@@ -10,6 +10,7 @@
 #include <libcparse/event.h>
 #include <libcparse/event/identifier.h>
 #include <libcparse/event/include.h>
+#include <libcparse/event/integer.h>
 #include <libcparse/status_codes.h>
 #include <stdlib.h>
 
@@ -20,9 +21,11 @@ CPARSE_IMPORT_event;
 CPARSE_IMPORT_event_copy;
 CPARSE_IMPORT_event_identifier;
 CPARSE_IMPORT_event_include;
+CPARSE_IMPORT_event_integer;
 
 static int event_copy_dispose_base(event_copy* cpy);
 static int event_copy_dispose_identifier(event_copy* cpy);
+static int event_copy_dispose_integer(event_copy* cpy);
 static int event_copy_dispose_include(event_copy* cpy);
 
 /**
@@ -50,6 +53,10 @@ int CPARSE_SYM(event_copy_release)(CPARSE_SYM(event_copy)* cpy)
 
             case CPARSE_EVENT_CATEGORY_IDENTIFIER:
                 release_retval = event_copy_dispose_identifier(cpy);
+                break;
+
+            case CPARSE_EVENT_CATEGORY_INTEGER_TOKEN:
+                release_retval = event_copy_dispose_integer(cpy);
                 break;
 
             case CPARSE_EVENT_CATEGORY_INCLUDE:
@@ -127,4 +134,18 @@ static int event_copy_dispose_identifier(event_copy* cpy)
 static int event_copy_dispose_include(event_copy* cpy)
 {
     return event_include_dispose(&cpy->detail.event_include);
+}
+
+/**
+ * \brief Dispose an integer event from this event copy.
+ *
+ * \param cpy                   Pointer to the \ref event_copy to release.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+static int event_copy_dispose_integer(event_copy* cpy)
+{
+    return event_integer_token_dispose(&cpy->detail.event_integer_token);
 }
