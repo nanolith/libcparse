4c47c43995e92f5127a2c46da027c9d5114278cd
Justin Handville <nanolith@gmail.com>
2024-10-18 22:58:05 -0400

    avl_tree export.

--- include/libcparse/util/avl_tree.h
+++ include/libcparse/util/avl_tree.h
@@ -22,6 +22,20 @@ extern "C" {
  */
 typedef struct CPARSE_SYM(avl_tree) CPARSE_SYM(avl_tree);
 
+/******************************************************************************/
+/* Start of public exports.                                                   */
+/******************************************************************************/
+
+#define __INTERNAL_CPARSE_IMPORT_util_avl_tree_sym(sym) \
+    CPARSE_BEGIN_EXPORT \
+    typedef CPARSE_SYM(avl_tree) sym ## avl_tree; \
+    CPARSE_END_EXPORT \
+    REQUIRE_SEMICOLON_HERE
+#define CPARSE_IMPORT_util_avl_tree_as(sym) \
+    __INTERNAL_CPARSE_IMPORT_util_avl_tree_sym(sym ## _)
+#define CPARSE_IMPORT_util_avl_tree \
+    __INTERNAL_CPARSE_IMPORT_util_avl_tree_sym()
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
