c5dfd92479357b347342eb93da7dddf3bc39db21
Justin Handville <nanolith@gmail.com>
2024-10-29 22:51:23 -0400

    Added return code check enforcement.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -460,7 +460,8 @@ CPARSE_SYM(event_init_for_token_tilde)(
  *      - STATUS_SUCCESS on success.
  *      - a non-zero error code on failure.
  */
-int CPARSE_SYM(event_init_for_token_question)(
+int FN_DECL_MUST_CHECK
+CPARSE_SYM(event_init_for_token_question)(
     CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
 
 /**
