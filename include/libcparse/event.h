271b6f494beb2fb1f19699b522a8254e494853f0
Justin Handville <nanolith@gmail.com>
2024-11-12 22:36:04 -0500

    Added return code check enforcement.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -2469,7 +2469,8 @@ CPARSE_SYM(event_init_for_exp_less_than_end)(
  *      - STATUS_SUCCESS on success.
  *      - a non-zero error code on failure.
  */
-int CPARSE_SYM(event_init_for_exp_greater_than_begin)(
+int FN_DECL_MUST_CHECK
+CPARSE_SYM(event_init_for_exp_greater_than_begin)(
     CPARSE_SYM(event)* ev, const CPARSE_SYM(cursor)* cursor);
 
 /**
