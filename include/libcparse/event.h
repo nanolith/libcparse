e9d58355c631922adaf16006348a50bb16a01b0e
Justin Handville <nanolith@gmail.com>
2024-11-16 22:55:14 -0500

    Added return code check enforcement.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -2954,7 +2954,8 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
     sym ## event_init_for_token_semicolon( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_token_semicolon)(x,y); } \
-    static inline int sym ## event_init_for_token_dot( \
+    static inline int FN_DECL_MUST_CHECK \
+    sym ## event_init_for_token_dot( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_token_dot)(x,y); } \
     static inline int sym ## event_init_for_token_arrow( \
