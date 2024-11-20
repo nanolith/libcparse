48a33b1a849545748d99a5467280b3b48e7804f3
Justin Handville <nanolith@gmail.com>
2024-11-20 18:29:21 -0500

    Added return code check enforcement.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -3126,7 +3126,8 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return \
                 CPARSE_SYM(event_init_for_token_preprocessor_id_elif)(x,y); } \
-    static inline int sym ## event_init_for_token_preprocessor_id_else( \
+    static inline int FN_DECL_MUST_CHECK \
+    sym ## event_init_for_token_preprocessor_id_else( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return \
                 CPARSE_SYM(event_init_for_token_preprocessor_id_else)(x,y); } \
