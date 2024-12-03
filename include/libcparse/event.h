a8f3949f673525339e000a5a34e35064b531589f
Justin Handville <nanolith@gmail.com>
2024-12-02 19:45:35 -0500

    Added return code check enforcement.

--- include/libcparse/event.h
+++ include/libcparse/event.h
@@ -3652,7 +3652,8 @@ CPARSE_SYM(event_get_cursor)(const CPARSE_SYM(event)* ev);
     sym ## event_init_for_exp_bitwise_and_end( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_exp_bitwise_and_end)(x,y); } \
-    static inline int sym ## event_init_for_exp_bitwise_xor_begin( \
+    static inline int FN_DECL_MUST_CHECK \
+    sym ## event_init_for_exp_bitwise_xor_begin( \
         CPARSE_SYM(event)* x, const CPARSE_SYM(cursor)* y) { \
             return CPARSE_SYM(event_init_for_exp_bitwise_xor_begin)(x,y); } \
     static inline int sym ## event_init_for_exp_bitwise_xor_end( \
