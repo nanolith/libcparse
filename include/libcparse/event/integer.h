db6c895e280887f8f49245f94c416f3f3b0648df
Justin Handville <nanolith@gmail.com>
2024-12-06 20:03:37 -0500

    Added return code check enforcement.

--- include/libcparse/event/integer.h
+++ include/libcparse/event/integer.h
@@ -122,7 +122,8 @@ CPARSE_SYM(event_integer_token_init_for_signed_long_long)(
  *      - STATUS_SUCCESS on success.
  *      - a non-zero failure code on failure.
  */
-int CPARSE_SYM(event_integer_token_init_for_unsigned_long_long)(
+int FN_DECL_MUST_CHECK
+CPARSE_SYM(event_integer_token_init_for_unsigned_long_long)(
     CPARSE_SYM(event_integer_token)* ev, const CPARSE_SYM(cursor)* cursor,
     unsigned long long val);
 
