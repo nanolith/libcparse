3245f6d9cfaf675bbbf51a6e415424f2eaa9d184
Justin Handville <nanolith@gmail.com>
2024-12-07 21:00:28 -0500

    Added return code check enforcement.

--- include/libcparse/event/integer.h
+++ include/libcparse/event/integer.h
@@ -384,7 +384,7 @@ int FN_DECL_MUST_CHECK CPARSE_SYM(event_integer_token_convert_to_int32)(
  *      - STATUS_SUCCESS on success.
  *      - a non-zero error code on failure.
  */
-int CPARSE_SYM(event_integer_token_convert_to_uint32)(
+int FN_DECL_MUST_CHECK CPARSE_SYM(event_integer_token_convert_to_uint32)(
     uint32_t* val, const CPARSE_SYM(event_integer_token)* ev);
 
 /**
