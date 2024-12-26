429a4e4b6a6e9d446ebc23869d955bba5805c14b
Justin Handville <nanolith@gmail.com>
2024-12-25 22:38:18 -0500

    Added return code check enforcement.

--- include/libcparse/message/subscription.h
+++ include/libcparse/message/subscription.h
@@ -204,7 +204,8 @@ CPARSE_SYM(message)* CPARSE_SYM(message_subscribe_upcast)(
             return \
                 CPARSE_SYM(message_subscribe_init_for_line_wrap_filter)( \
                     x,y); } \
-    static inline int sym ## message_subscribe_init_for_nl_ws_filter(\
+    static inline int FN_DECL_MUST_CHECK \
+    sym ## message_subscribe_init_for_nl_ws_filter(\
         CPARSE_SYM(message_subscribe)* x, CPARSE_SYM(event_handler)* y) { \
             return \
                 CPARSE_SYM(message_subscribe_init_for_nl_ws_filter)(x,y); } \
